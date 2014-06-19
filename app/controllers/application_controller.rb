3d8ee8e34b349fababa691a03072ff247dda7c35
Mac Eisenberg & Dustin Brown <student@epicodus.com>
2014-06-19 14:19:41 -0700

    adds payment#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_params, if: :devise_controller?
+
+protected
+  def configure_permitted_params
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'payments#index'
   devise_for :users
+  resources :payments
 end
