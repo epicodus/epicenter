23a015944021c8dde43904100b0450e452da3d1a
Mac Eisenberg & Dustin Brown <student@epicodus.com>
2014-06-25 12:39:59 -0700

    remove warnings when running rspec

--- spec/support/matchers/request_matchers.rb
+++ spec/support/matchers/request_matchers.rb
@@ -1,5 +1,5 @@
 RSpec::Matchers.define :have_submit_button do |value|
   match do |actual|
-    actual.should have_selector("input[type=submit][value='#{value}']")
+    expect(actual).to have_selector("input[type=submit][value='#{value}']")
   end
 end
