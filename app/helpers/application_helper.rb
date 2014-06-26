cc85113887d5eeb8dc9ec72662d000b95e86fc95
Mac Eisenberg & Dustin Brown <student@epicodus.com>
2014-06-26 13:41:47 -0700

    add helpers for displaying flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def flash_notice(flash)
+    if flash[:notice]
+      content_tag :div, class: 'alert alert-success' do
+        flash[:notice]
+      end
+    elsif flash[:alert]
+      content_tag :div, class: 'alert alert-warning' do
+        flash[:alert]
+      end
+    end
+  end
 end
