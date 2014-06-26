4c04848ed94cd6fe2abfb7510252c135e87a4e1d
Mac Eisenberg & Dustin Brown <student@epicodus.com>
2014-06-26 11:39:02 -0700

    validate presence of subscription and amount on payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,4 @@
 class Payment < ActiveRecord::Base
   belongs_to :subscription
+  validates_presence_of :amount, :subscription_id
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,3 +1,7 @@
+require 'rails_helper'
+
 describe Payment do
   it { should belong_to :subscription }
+  it { should validate_presence_of :amount }
+  it { should validate_presence_of :subscription_id }
 end
