a2ac531314f75ebd4eafb4da38633aea666ae93b
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2014-07-17 12:13:52 -0700

    clean failing spec, uncomment and make pending

--- spec/features/subscriptions_pages_spec.rb
+++ spec/features/subscriptions_pages_spec.rb
@@ -2,23 +2,21 @@ require 'rails_helper'
 
 feature 'User creates a subscription' do
   before do
-    @user = create(:user)
-    sign_in @user
+    user = create(:user)
+    sign_in user
     visit new_subscription_path
-    fill_in 'Name on account', with: @user.name
+    fill_in 'Name on account', with: user.name
   end
 
-  context 'with valid information', js: true do
-
-    before do
-      fill_in 'Bank account number', with: '123456789'
-      fill_in 'Routing number', with: '321174851'
-      click_on 'Add bank account'
-    end
+  xscenario 'with valid information', js: true do
+    fill_in 'Bank account number', with: '123456789'
+    fill_in 'Routing number', with: '321174851'
+    click_on 'Verify bank account'
+    page.save_screenshot('tmp/screenshots/valid_bank_account.png')
+    expect(page).to have_content 'verify the deposits'
   end
 
   scenario 'with missing account number', js: true do
-
     fill_in 'Routing number', with: '321174851'
     click_on 'Verify bank account'
     within 'div.error' do
@@ -27,7 +25,6 @@ feature 'User creates a subscription' do
   end
 
   scenario 'with invalid routing number', js: true do
-
     fill_in 'Bank account number', with: '123456789'
     fill_in 'Routing number', with: '1234568'
     click_on 'Verify bank account'
