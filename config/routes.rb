e6ad3aae7c142cb3da9954d213c1d0e5f968d738
michael <student@epicodus.com>
2014-07-23 16:11:24 -0700

    switch to singular route for bank account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   devise_for :users
-  resources :bank_accounts, only: [:new, :create]
+  resource :bank_account, only: [:new, :create]
   resource :verification, only: [:edit, :update]
   resources :payments, only: [:index]
 end
