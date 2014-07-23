dfc0e4f55ce4a5cefb36b8c00f31b7f9813c8d27
michael <student@epicodus.com>
2014-07-23 15:12:13 -0700

    reorganize feature specs around signing in

--- spec/features/users_pages_spec.rb
+++ spec/features/users_pages_spec.rb
@@ -21,25 +21,48 @@ feature 'User signs up' do
 end
 
 feature "User signs in" do
-  subject { page }
-
-  context "with unverified account", :vcr do
-    before do
-      user = create(:user)
-      bank_account = FactoryGirl.create(:bank_account)
-      user.bank_account = bank_account
+  context "before entering bank account info" do
+    it "takes them to the page to enter their bank account info" do
+      user = FactoryGirl.create(:user)
       sign_in user
+      expect(page).to have_content "Bank account information"
     end
-
-    it { should have_content "First deposit amount" }
   end
 
-  context "with verified account", :vcr do
-    before do
+  context "after entering bank account info but before verifying" do
+    it "takes them to the page to verify their account" do
+      user = FactoryGirl.create(:user)
+      bank_account = FactoryGirl.create(:bank_account, user: user)
+      sign_in user
+      expect(page).to have_content "Confirm your account"
+    end
+  end
+
+  context "after verifying their account", :vcr do
+    it "shows them their payment history" do
       user = FactoryGirl.create(:user_with_verified_bank_account)
       sign_in user
+      expect(page).to have_content "Your payments"
     end
+  end
+end
 
-    it { should have_content "Your payments" }
+feature 'Guest not signed in' do
+  subject { page }
+  
+  context 'visits new subscrition path' do
+    before { visit new_bank_account_path }
+    it { should have_content 'You need to sign in'}
+  end
+
+  context 'visits edit verification path' do
+    before { visit edit_verification_path }
+    it { should have_content 'You need to sign in' }
+  end
+
+  context 'visits user show path' do
+    let(:user) { create(:user) }
+    before { visit user_path(user) }
+    it { should have_content 'You need to sign in' }
   end
 end
