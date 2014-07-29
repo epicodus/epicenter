a050d467c7341bc63cb1cbd2db0538caeb3c3863
michael <michaelrkn@gmail.com>
2014-07-28 17:19:05 -0700

    record slow test with vcr

--- spec/features/payments_pages_spec.rb
+++ spec/features/payments_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'User views payment history' do
-  scenario 'and sees the payments they have made' do
+  scenario 'and sees the payments they have made', :vcr do
     payment = FactoryGirl.create(:payment)
     user = payment.bank_account.user
     sign_in user
