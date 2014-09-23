d03c45e6e52c5a9571137f3c8fc035f7ba1a93c0
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-23 11:04:37 -0700

    add github auth to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, presence: true
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :bank_account
