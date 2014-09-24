66a04f00e56f960f8bb042ab0feddd1a75c333c8
Austin Steward <student@epicodus.com>
2014-09-24 12:00:31 -0700

    update navbar routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ActiveRecord::Base
   has_one :bank_account
   has_many :payments, through: :bank_account
 
+  scope :teachers, -> { where(admin: true) }
+  scope :students, -> { where(admin: false) }
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
