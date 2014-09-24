ad0d59e7aeaf0056093be0aa1483cb00758f4585
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-24 16:28:03 -0700

    add create grades per requirement functionality

---
+++ app/controllers/grades_controller.rb
@@ -0,0 +1,22 @@
+class GradesController < ApplicationController
+  def new
+    @grade = Grade.new
+  end
+
+  def create
+    @grade = Grade.new(grade_params)
+    @submission = Submission.find(params[:grade][:submission_id])
+    @assessment = Assessment.find(@submission.assessment_id)
+      if @grade.save
+        respond_to do |format|
+          format.html { redirect_to assessment_submission_path(@assessment, @submission) }
+          format.js
+        end
+      end
+  end
+
+private
+  def grade_params
+    params.require(:grade).permit(:comment, :score, :requirement_id, :submission_id)
+  end
+end
