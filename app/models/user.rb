7827982af76a805cfc7e6d8b0b61f9ae2b5a2321
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-24 13:44:46 -0700

    update user model submission association

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   has_one :bank_account
   has_many :payments, through: :bank_account
+  has_many :submissions
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
