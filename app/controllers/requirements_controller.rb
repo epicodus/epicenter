8f3aaa7f558cfb79b2f5c2bca5447f2a220338ff
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-24 11:16:31 -0700

    add edit requirement feature

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -20,9 +20,19 @@ class RequirementsController < ApplicationController
   end
 
   def edit
+    @assessment = Assessment.find(params[:assessment_id])
+    @requirement = @assessment.requirements.find(params[:id])
   end
 
   def update
+    @assessment = Assessment.find(params[:assessment_id])
+    @requirement = @assessment.requirements.find(params[:id])
+    if @requirement.update(requirement_params)
+      respond_to do |format|
+        format.html { redirect_to @assessment }
+        format.js
+      end
+    end
   end
 
   def destroy
