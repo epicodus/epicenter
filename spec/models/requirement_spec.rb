0ae94fc76c35a377e7d611d67de5fe4b0d88fa74
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-24 13:40:54 -0700

    add grade model and associations

--- spec/models/requirement_spec.rb
+++ spec/models/requirement_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Requirement, :type => :model do
   it { should validate_presence_of :content }
   it { should belong_to :assessment }
+  it { should have_many :grades }
 end
