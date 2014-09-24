8c5c2ad9fcbfc9963b0634b08fb00e01a871a13a
Austin Steward <student@epicodus.com>
2014-09-24 15:08:19 -0700

    Add assessment requirements to student show page

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -2,9 +2,9 @@
     $(".student-reqs").click(function() {
         $(".reqs-container").toggle();
         if ($(".reqs-container").is(":visible")) {
-            $(".reqs-container").slideDown();
+            $(".reqs-container").fadeIn();
         } else {
-            $(".reqs-container").slideUp();
+            $(".reqs-container").fadeOut();
         }
         return false;
     });
