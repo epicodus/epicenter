730a0f1c86ec0f96d48fc188f294844f62e0d2ab
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-24 10:33:09 -0700

    update layout for requirement edit and delete buttons

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140923230546) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,18 +43,18 @@ ActiveRecord::Schema.define(version: 20140923230546) do
     t.string   "payment_uri"
   end
 
-  create_table "submissions", force: true do |t|
-    t.integer "user_id"
-    t.string  "link"
-    t.text    "note"
-    t.integer "assessment_id"
-  end
-
   create_table "requirements", force: true do |t|
     t.string   "content"
     t.integer  "assessment_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "submissions", force: true do |t|
+    t.integer "user_id"
+    t.string  "link"
+    t.text    "note"
+    t.integer "assessment_id"
   end
 
   create_table "users", force: true do |t|
