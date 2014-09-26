4af0c386b28c3436cc24628a74806bba1b450efc
Austin Steward & Ellen Krause Lehman <student@epicodus.com>
2014-09-26 11:11:09 -0700

    test methods to count total 3, 2, 1 point grades

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,5 +41,38 @@ describe User do
       expect(student.grade_four).to eq 2
     end
   end
+
+  describe 'grade_three' do
+    it 'returns the count of three point grades for all submissions' do
+      student = FactoryGirl.create(:user, admin: false)
+      assessment = FactoryGirl.create(:assessment)
+      submission = FactoryGirl.create(:submission, assessment_id: assessment.id, graded: true, user_id: student.id)
+      grade = FactoryGirl.create(:grade, submission_id: submission.id, score: 3)
+      grade2 = FactoryGirl.create(:grade, submission_id: submission.id, score: 3)
+      expect(student.grade_three).to eq 2
+    end
+  end
+
+  describe 'grade_two' do
+    it 'returns the count of two point grades for all submissions' do
+      student = FactoryGirl.create(:user, admin: false)
+      assessment = FactoryGirl.create(:assessment)
+      submission = FactoryGirl.create(:submission, assessment_id: assessment.id, graded: true, user_id: student.id)
+      grade = FactoryGirl.create(:grade, submission_id: submission.id, score: 2)
+      grade2 = FactoryGirl.create(:grade, submission_id: submission.id, score: 2)
+      expect(student.grade_two).to eq 2
+    end
+  end
+
+  describe 'grade_one' do
+    it 'returns the count of one point grades for all submissions' do
+      student = FactoryGirl.create(:user, admin: false)
+      assessment = FactoryGirl.create(:assessment)
+      submission = FactoryGirl.create(:submission, assessment_id: assessment.id, graded: true, user_id: student.id)
+      grade = FactoryGirl.create(:grade, submission_id: submission.id, score: 1)
+      grade2 = FactoryGirl.create(:grade, submission_id: submission.id, score: 1)
+      expect(student.grade_one).to eq 2
+    end
+  end
 end
 
