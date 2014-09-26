047b32d075511b6c3a9efe0f61b8162b4d4d3f51
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-26 09:24:29 -0700

    add grade belongs to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925224905) do
+ActiveRecord::Schema.define(version: 20140926162147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140925224905) do
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "payments", force: true do |t|
