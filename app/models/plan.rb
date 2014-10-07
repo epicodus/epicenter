ed4445aeab074bc46137c143a77caeb77a3b803e
Mac Eisenberg <maceisen@gmail.com>
2014-10-07 08:44:49 -0700

    validate presence of name for Plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,6 @@
 class Plan < ActiveRecord::Base
   has_many :users
+  validates :name, presence: true
   validates :recurring_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :upfront_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Plan do
   it { should have_many :users }
+  it { should validate_presence_of :name }
   it { should validate_presence_of :recurring_amount }
   it { should validate_presence_of :upfront_amount }
   it { should validate_numericality_of :upfront_amount }
