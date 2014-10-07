54f91071941327c941327c38fa0cda6ddbf9f6d8
Dustin <dbrown9@gmail.com>
2014-10-07 10:01:16 -0700

    better validate attendance_record to unique user_id per day
    The old validation required instantiating the associated user model,
    then all of its other attendance records and making sure the created_at
    doesn't match the current day. This created a lot of overhead for what
    should be a simple query. The new way uses a condition to scope the
    uniqueness check on user_id to just records for today.

--- app/models/attendance_record.rb
+++ app/models/attendance_record.rb
@@ -1,13 +1,7 @@
 class AttendanceRecord < ActiveRecord::Base
   scope :today, -> { where('DATE(created_at) = ?', Date.today) }
-  validates :user_id, presence: true
-  validate :record_has_not_been_created_today
+  validates :user_id, presence: true, uniqueness: {conditions: -> { where('DATE(created_at) = ?', Date.today) }}
 
   belongs_to :user
 
-  def record_has_not_been_created_today
-    if user && user.attendance_records.today.exists?
-      errors.add(:user_id, 'This user has already signed in today')
-    end
-  end
 end
