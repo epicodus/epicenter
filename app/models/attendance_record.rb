5bf8fc9777778c024d1a3553d43c69023e2840f8
Dustin <dbrown9@gmail.com>
2014-10-07 11:51:28 -0700

    fix syntax on lambda

--- app/models/attendance_record.rb
+++ app/models/attendance_record.rb
@@ -1,6 +1,6 @@
 class AttendanceRecord < ActiveRecord::Base
   scope :today, -> { where('DATE(created_at) = ?', Date.today) }
-  validates :user_id, presence: true, uniqueness: {conditions: -> { where('DATE(created_at) = ?', Date.today) }}
+  validates :user_id, presence: true, uniqueness: { conditions: -> { where('DATE(created_at) = ?', Date.today) } }
 
   belongs_to :user
 
