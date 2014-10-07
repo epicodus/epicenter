443a6788a845212bed7a02a07c8966e4c787f997
Dustin <dbrown9@gmail.com>
2014-10-07 12:31:54 -0700

    remove before blocks to make tests more readable

--- spec/features/attendance_record_pages_spec.rb
+++ spec/features/attendance_record_pages_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
 feature 'creating an attendance record' do
-  before { @user = FactoryGirl.create(:user) }
 
   scenario 'correctly' do
+    FactoryGirl.create(:user)
     visit '/attendance'
     click_button("I'm here")
     expect(page).to have_content "Welcome"
   end
 
   scenario 'after having already created one today' do
-    AttendanceRecord.create(user: @user)
+    user = FactoryGirl.create(:user)
+    AttendanceRecord.create(user: user)
     visit '/attendance'
     expect(page).not_to have_content "I'm here"
   end
 end
 
 feature 'destroying an attendance record' do
-  before { @user = FactoryGirl.create(:user) }
 
   scenario 'after accidentally creating one' do
+    FactoryGirl.create(:user)
     visit '/attendance'
     click_button("I'm here")
     click_link("Not you?")
