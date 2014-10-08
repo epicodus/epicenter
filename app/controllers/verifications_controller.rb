c463480fbf5f2433bd6e70e4402ab8faba19e9e0
Mac Eisenberg <maceisen@gmail.com>
2014-10-08 14:41:15 -0700

    redirect to upfront payment after verification

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -9,7 +9,7 @@ class VerificationsController < ApplicationController
     @verification = Verification.new(params[:verification].merge(bank_account: current_user.bank_account))
     if @verification.confirm
       flash[:notice] = "Your bank account has been confirmed."
-      redirect_to payments_path
+      redirect_to new_upfront_payment_path
     else
       render :edit
     end
