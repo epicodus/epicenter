b3f835e848dcebc2b22b830db378e40118a214f4
Mac Eisenberg <maceisen@gmail.com>
2014-10-08 14:29:50 -0700

    ensure upfront payment is due

--- app/controllers/upfront_payments_controller.rb
+++ app/controllers/upfront_payments_controller.rb
@@ -1,5 +1,6 @@
 class UpfrontPaymentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_upfront_payment_is_due
 
   def new
     @payment = Payment.new(amount: current_user.plan.upfront_amount)
@@ -9,5 +10,10 @@ class UpfrontPaymentsController < ApplicationController
     current_user.bank_account.make_upfront_payment
     flash[:notice] = "Thank You! Your upfront payment has been made."
     redirect_to payments_path
+  end
+
+private
+  def ensure_upfront_payment_is_due
+    redirect_to root_path if !current_user.upfront_payment_due?
   end
 end
