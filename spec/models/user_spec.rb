c1f57ec2ad4a89a9bbaab8ed7845671b8df3ef15
Dustin <dbrown9@gmail.com>
2014-10-08 09:34:42 -0700

    add additional model tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,18 @@ describe User do
   it { should have_one :bank_account }
   it { should have_many :payments }
   it { should belong_to :plan }
-end
+  it { should have_many :attendance_records }
 
+  describe '#signed_in_today?' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    it 'is false if the user has not signed in today' do
+      expect(user.signed_in_today?).to eq false
+    end
+    
+    it 'is true if the user has already signed in today' do
+      attendance_record = FactoryGirl.create(:attendance_record, user: user)
+      expect(user.signed_in_today?).to eq true
+    end
+  end
+end
