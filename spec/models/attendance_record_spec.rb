677ec703f07a49b313f24a90e890a3130b3a0767
Dustin <dbrown9@gmail.com>
2014-10-13 17:10:09 -0700

    add tardy boolean to attendance_record
    Created a after validation hook that will mark an attendance record as
    tardy if it is after the start time. Doing this will means we have records
    of tardiness even if we change what the start time is.

--- spec/models/attendance_record_spec.rb
+++ spec/models/attendance_record_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe AttendanceRecord do
+  include ActiveSupport::Testing::TimeHelpers
   it { should belong_to :user }
   it { should validate_presence_of :user_id }
 
@@ -8,5 +9,14 @@ describe AttendanceRecord do
     first_attendance_record = FactoryGirl.create(:attendance_record)
     second_attendance_record = FactoryGirl.build(:attendance_record, user: first_attendance_record.user)
     expect(second_attendance_record.valid?).to eq false
+  end
+
+  it 'is counted as tardy if the student checks in more than 2 minutes after the start of class' do
+    travel_to Time.new(2015, 01, 05, 8, 55, 00)
+    on_time_record = FactoryGirl.create(:attendance_record)
+    travel 7.minutes
+    tardy_record = FactoryGirl.create(:attendance_record)
+    expect(on_time_record.tardy).to eq false
+    expect(tardy_record.tardy).to eq true
   end
 end
