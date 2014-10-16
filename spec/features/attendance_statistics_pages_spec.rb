e2cd03695b69e1b2054f16dbb193b41ee84b67eb
Dustin <dbrown9@gmail.com>
2014-10-15 17:06:29 -0700

    add specs to ensure graphs render on attendace statistics pages

--- spec/features/attendance_statistics_pages_spec.rb
+++ spec/features/attendance_statistics_pages_spec.rb
@@ -7,4 +7,18 @@ feature 'attendance statistics page' do
     visit cohort_attendance_statistics_path(cohort)
     expect(page).to have_content 'Attendance statistics'
   end
+
+  scenario 'shows number of attendances chart', js: true do
+    user = FactoryGirl.create(:user, cohort: cohort)
+    FactoryGirl.create(:attendance_record, user: user)
+    visit cohort_attendance_statistics_path(cohort)
+    expect(page).to have_content 'Number of students present'
+  end
+
+  scenario 'shows chart of breakdown of student attendances', js: true do
+    user = FactoryGirl.create(:user, cohort: cohort)
+    FactoryGirl.create(:attendance_record, user: user)
+    visit cohort_attendance_statistics_path(cohort)
+    expect(page).to have_content user.name
+  end
 end
