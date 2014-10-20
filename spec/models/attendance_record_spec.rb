3f88402a210f9e0829ad2ee3fd7311fdaa9820ea
Dustin Brown <dbrown9@gmail.com>
2014-10-20 10:10:42 -0700

    add specs for attendance_record model

--- spec/models/attendance_record_spec.rb
+++ spec/models/attendance_record_spec.rb
@@ -4,10 +4,32 @@ describe AttendanceRecord do
   it { should belong_to :user }
   it { should validate_presence_of :user_id }
 
-  it 'validates that an attendance record has not already been created for today' do
-    first_attendance_record = FactoryGirl.create(:attendance_record)
-    second_attendance_record = FactoryGirl.build(:attendance_record, user: first_attendance_record.user)
-    expect(second_attendance_record.valid?).to eq false
+  describe 'uniqueness validation for users' do
+    it 'validates that an attendance record for a user has not already been created for today' do
+      first_attendance_record = FactoryGirl.create(:attendance_record)
+      second_attendance_record = FactoryGirl.build(:attendance_record, user: first_attendance_record.user)
+      expect(second_attendance_record.valid?).to eq false
+    end
+
+    it 'allows multiple users to check in on a single day' do
+      first_user = FactoryGirl.create(:user)
+      second_user = FactoryGirl.create(:user)
+      first_attendance_record = FactoryGirl.create(:attendance_record, user: first_user)
+      second_attendance_record = FactoryGirl.build(:attendance_record, user: second_user)
+      expect(second_attendance_record.valid?).to eq true
+    end
+  end
+
+  describe '.today' do
+    include ActiveSupport::Testing::TimeHelpers
+
+    it 'returns all the attendance records for today' do
+      past_attendance_record = FactoryGirl.create(:attendance_record)
+      travel_to Date.today + 1.day do
+        current_attendance_record = FactoryGirl.create(:attendance_record)
+        expect(AttendanceRecord.today).to eq [current_attendance_record]
+      end
+    end
   end
 
   describe '#tardy' do
