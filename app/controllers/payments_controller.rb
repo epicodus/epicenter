cb2afca2144f631f7ed0ef70fe9d11df1c67db6d
Mac Eisenberg <maceisen@gmail.com>
2014-10-21 10:23:37 -0700

    move start payment buttons to payments#index

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,5 +3,8 @@ class PaymentsController < ApplicationController
 
   def index
     @payments = current_user.payments
+    @payment = Payment.new
+    @recurring_amount = current_user.primary_payment_method.calculate_charge(current_user.plan.recurring_amount)
+    @upfront_amount = current_user.primary_payment_method.calculate_charge(current_user.plan.upfront_amount)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Rails.application.routes.draw do
   get 'attendance', to: 'attendance_records#index', as: 'attendance'
   get 'payment_method', to: 'static_pages#payment_method', as: 'payment_method'
 
-
   devise_for :users
 
   resource :bank_account, only: [:new, :create]
   resource :credit_card, only: [:new, :create]
   resource :verification, only: [:edit, :update]
   resources :payments, only: [:index]
-  resources :upfront_payments, only: [:new, :create]
-  resources :recurring_payments, only: [:new, :create]
+  resources :upfront_payments, only: [:create]
+  resources :recurring_payments, only: [:create]
   resources :attendance_records, only: [:create, :destroy]
 end
