96bfa70da666b0040bdcbebdef1261b814f01c4f
Mac Eisenberg <maceisen@gmail.com>
2014-10-20 17:09:39 -0700

    add backwards-compatible migration for polymorphic associations

--- db/migrate/20141020190337_add_polymorphic_associations_to_payment.rb
+++ db/migrate/20141020190337_add_polymorphic_associations_to_payment.rb
@@ -1,6 +1,23 @@
 class AddPolymorphicAssociationsToPayment < ActiveRecord::Migration
+  class Payment < ActiveRecord::Base
+    belongs_to :user
+  end
+
+  class User < ActiveRecord::Base
+    has_one :bank_account
+  end
+
+  class BankAccount < ActiveRecord::Base
+    belongs_to :user
+  end
+
   def change
     add_column :payments, :payment_method_id, :integer
     add_column :payments, :payment_method_type, :string
+    Payment.all.each do |payment|
+      payment.payment_method_id = payment.user.bank_account.id
+      payment.payment_method_type = 'BankAccount'
+      payment.save
+    end
   end
 end
