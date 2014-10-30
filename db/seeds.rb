9cb672e160327b724b922af6f3a291fc14777f4b
Mac Eisenberg <maceisen@gmail.com>
2014-10-30 11:40:20 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Payment Plans
+
+plans = [
+  ["Spring 2014 loan", 0, 625_00, 5000_00],
+  ["Summer 2014 loan", 0, 600_00, 5000_00],
+  ["Winter 2015 loan", 200_00, 600_00, 5000_00],
+  ["Winter 2015 up-front", 3400_00, 0, 3400_00]
+]
+
+plans.each do |name, upfront_amount, recurring_amount, total_amount|
+  Plan.create(name: name, upfront_amount: upfront_amount, recurring_amount: recurring_amount, total_amount: total_amount)
+end
+
+
+#Cohorts / Classes
+
+cohorts = [
+  ["Spring 2014", Date.parse("January 17, 2014"), Date.parse("January 18, 2014")],
+  ["Summer 2014", Date.parse("January 17, 2014"), Date.parse("January 18, 2014")],
+  ["Winter 2015", Date.parse("January 17, 2014"), Date.parse("January 18, 2014")],
+]
+
+cohorts.each do |description, start_date, end_date|
+  Cohort.create(description: description, start_date: start_date, end_date: end_date)
+end
