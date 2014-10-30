b429e1d4c2ca761cc81355a23bd4db12e6afc393
Mac Eisenberg <maceisen@gmail.com>
2014-10-30 11:00:33 -0700

    add database index to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030173226) do
+ActiveRecord::Schema.define(version: 20141030175701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20141030173226) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["plan_id"], name: "index_users_on_plan_id", using: :btree
+  add_index "users", ["recurring_active"], name: "index_users_on_recurring_active", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
