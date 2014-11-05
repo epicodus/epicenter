80110ac405fabdd5731e9d740b41c77562968cf7
Mac Eisenberg <maceisen@gmail.com>
2014-11-05 11:33:52 -0800

    show verification status of payment method

--- spec/features/payment_methods_pages_spec.rb
+++ spec/features/payment_methods_pages_spec.rb
@@ -10,27 +10,58 @@ feature 'Student views payment methods page' do
     end
   end
 
+  context 'after a bank account is added but not verified', :vcr do
+    it "shows payment method as pending and non-primary" do
+      student = FactoryGirl.create(:user_with_unverified_bank_account)
+      sign_in student
+      visit payment_methods_path
+      expect(page).to have_content "xxxxxx0002"
+      expect(page).to have_content "Bank account"
+      expect(page).to have_content "Pending"
+      expect(page).to have_content "Make Primary"
+    end
+  end
+
   context 'after a primary payment method has been added', :vcr do
-    it "shows payment method as primary" do
+    it "shows credit card as primary and verified" do
       student = FactoryGirl.create(:user_with_credit_card)
       sign_in student
       visit payment_methods_path
       expect(page).to have_content "xxxxxxxxxxxx1111"
       expect(page).to have_content "✓"
       expect(page).to have_content "Credit card"
+      expect(page).to have_content "Verified"
+      expect(page).to_not have_content "Make Primary"
+    end
+
+    it "shows verified bank account as primary and verified" do
+      student = FactoryGirl.create(:user_with_verified_bank_account)
+      sign_in student
+      visit payment_methods_path
+      expect(page).to have_content "xxxxxx0002"
+      expect(page).to have_content "✓"
+      expect(page).to have_content "Bank account"
+      expect(page).to have_content "Verified"
       expect(page).to_not have_content "Make Primary"
     end
   end
 
   context 'after an additional payment method has been added', :vcr do
-    it "shows additional payment method as non-primary" do
+    before do
       student = FactoryGirl.create(:user_with_credit_card)
       bank_account = FactoryGirl.create(:bank_account, student: student)
       sign_in student
       visit payment_methods_path
+    end
+
+    it "shows additional payment method as non-primary" do
       expect(page).to have_content "xxxxxx0002"
       expect(page).to have_content "Bank account"
       expect(page).to have_content "Make Primary"
+    end
+
+    it "shows unverified bank account as pending" do
+      expect(page).to have_content "Pending"
     end
   end
 end
