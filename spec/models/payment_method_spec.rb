eda14ad739947d1dd43d39f83803afbe39bb3d97
Mac Eisenberg <maceisen@gmail.com>
2014-11-12 12:04:56 -0800

    add vcr to spec

--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -5,7 +5,7 @@ describe PaymentMethod do
   it { should have_many :payments }
 
   describe '.not_verified_first' do
-    it 'returns payment methods ordered by not verified' do
+    it 'returns payment methods ordered by not verified', :vcr do
       credit_card = FactoryGirl.create(:credit_card)
       bank_account = FactoryGirl.create(:bank_account)
       expect(PaymentMethod.not_verified_first).to eq [bank_account, credit_card]
