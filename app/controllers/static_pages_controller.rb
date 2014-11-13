671b09751f7bb7ce55595348dbc112ccac22aab8
Mac Eisenberg <maceisen@gmail.com>
2014-11-13 15:08:04 -0800

    redirect user from visiting homepage if logged in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+  before_filter :redirect_if_logged_in
+
   def index
+  end
+
+private
+  def redirect_if_logged_in
+    redirect_to after_sign_in_path_for(current_user) if current_user
   end
 end
