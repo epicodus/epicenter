36d1d11726d45a143d75598f08bbfc9b7d091cbc
Dustin Brown <dbrown9@gmail.com>
2015-01-21 09:30:00 -0800

    change redirect for attendance record amendments to the attendance record amendment new page

--- app/controllers/attendance_record_amendments_controller.rb
+++ app/controllers/attendance_record_amendments_controller.rb
@@ -9,7 +9,7 @@ class AttendanceRecordAmendmentsController < ApplicationController
     @attendance_record_amendment = AttendanceRecordAmendment.new(attendance_record_amendment_params)
     if @attendance_record_amendment.save
       student = Student.find(params[:attendance_record_amendment][:student_id])
-      redirect_to root_path, notice: "#{student.name}'s attendance record has been amended."
+      redirect_to new_attendance_record_amendment_path, notice: "#{student.name}'s attendance record has been amended."
     else
       render :new, alert: 'Something went wrong. Please try again.'
     end
