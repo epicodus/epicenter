077736a58d8bc762cd33c222a70ac6c63e079bb0
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2015-01-22 18:28:57 -0800

    rescue and surface errors creating bank accounts

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,9 +1,14 @@
 class BankAccount < PaymentMethod
-  before_create :create_verification
   before_create :get_last_four_string
+  before_create :create_verification
 
   def fetch_balanced_account
-    Balanced::BankAccount.fetch(account_uri)
+    begin
+      Balanced::BankAccount.fetch(account_uri)
+    rescue Balanced::Error => exception
+      errors.add(:base, exception.description)
+      false
+    end
   end
 
   def calculate_fee(amount)
@@ -22,6 +27,10 @@ private
   end
 
   def get_last_four_string
-    self.last_four_string = fetch_balanced_account.account_number
+    if balanced_account = fetch_balanced_account
+      self.last_four_string = balanced_account.account_number
+    else
+      false
+    end
   end
 end
