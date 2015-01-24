09bc55f516b15dfc6eb74cc0a64814c4368b90f4
Dustin Brown <dbrown9@gmail.com>
2015-01-23 21:01:38 -0800

    refactor AttendanceRecordAmendmen to isolate dependency on AttendanceRecord class

--- app/models/attendance_record_amendment.rb
+++ app/models/attendance_record_amendment.rb
@@ -1,26 +1,33 @@
 class AttendanceRecordAmendment
   include ActiveModel::Model
 
-  attr_reader :student_id, :date, :status
+  attr_reader :student_id, :date, :status, :attendance_record
 
   def initialize(attributes={})
     @student_id = attributes[:student_id]
     @date = attributes[:date]
     @status = attributes[:status]
+    @attendance_record = fetch_attendance_record
   end
 
-  def save
-    if @status == "Absent"
-      destroy_attendance_record
-    else
-      attendance_record = AttendanceRecord.find_or_initialize_by(student_id: @student_id, date: @date)
-      attendance_record.tardy = @status == "On time" ? false : true
-      attendance_record.save
+  def amend
+    case status
+    when "On time"
+      attendance_record.update(tardy: false)
+    when "Tardy"
+      attendance_record.update(tardy: true)
+    when "Absent"
+      attendance_record.try(:destroy)
     end
   end
 
+private
+
+  def fetch_attendance_record
+    AttendanceRecord.find_or_initialize_by(student_id: student_id, date: date)
+  end
+
   def destroy_attendance_record
-    attendance_record = AttendanceRecord.find_by(student_id: @student_id, date: @date)
-    attendance_record.destroy unless attendance_record.nil?
+    attendance_record.try(:destroy)
   end
 end
--- spec/models/attendance_record_amendment_spec.rb
+++ spec/models/attendance_record_amendment_spec.rb
@@ -1,36 +1,36 @@
 describe AttendanceRecordAmendment do
-  describe '#save' do
+  describe '#amend' do
     let(:student) { FactoryGirl.create(:student) }
 
     it 'creates a new attendance record for the student if they did not have one for the given date' do
       attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'On time')
-      attendance_record_amendment.save
+      attendance_record_amendment.amend
       expect(student.attendance_records.first.date).to eq(Date.today)
     end
 
     it 'sets tardy to false if the status is "On time"' do
       attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'On time')
-      attendance_record_amendment.save
+      attendance_record_amendment.amend
       expect(student.attendance_records.first.tardy).to eq(false)
     end
 
     it 'sets tardy to true if the status is "Tardy"' do
       attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'Tardy')
-      attendance_record_amendment.save
+      attendance_record_amendment.amend
       expect(student.attendance_records.first.tardy).to eq(true)
     end
 
     it 'updates the status if an attendance record alread exists for the given day' do
       FactoryGirl.create(:attendance_record, student: student, date: Date.today, tardy: true)
       attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'On time')
-      attendance_record_amendment.save
+      attendance_record_amendment.amend
       expect(student.tardies).to eq 0
     end
 
     it 'destroys an existing attendance record for the given date if the status is "Absent"' do
       FactoryGirl.create(:attendance_record, student: student, date: Date.today)
       attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'Absent')
-      attendance_record_amendment.save
+      attendance_record_amendment.amend
       expect(student.attendance_records.count).to eq 0
     end
   end
