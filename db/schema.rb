69cf7fb49e04703707162df55e9116f6261376cc
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-01 13:37:28 -0700

    add DeviseInvitable to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331175425) do
+ActiveRecord::Schema.define(version: 20150401203212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
