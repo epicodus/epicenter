de0a6b856ae68964ed97ff7dbb17576dd26908af
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-01 14:32:54 -0700

    include devise invitable and include the Inviter module into the admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,8 @@ class Admin < User
   belongs_to :current_cohort, class_name: 'Cohort'
 
   before_create :assign_current_cohort
+  devise :database_authenticatable, :validatable, :invitable
+  include DeviseInvitable::Inviter
 
 private
 
