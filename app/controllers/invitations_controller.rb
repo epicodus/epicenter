304c60644e49361984592af2356b1bf730c48b2a
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-01 20:51:18 -0700

    after invite path method does not route accordingly with role checking, reverting back to root path for now

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,11 +1,7 @@
 class InvitationsController < Devise::InvitationsController
 
   def after_invite_path_for(user)
-    if user.is_a? Admin
-      cohort_assessments_path(user.current_cohort)
-    elsif user.is_a? Student
-      user.class_in_session? ? cohort_assessments_path(user.cohort) : proper_payments_path(user)
-    end
+    root_path
   end
 
 end
