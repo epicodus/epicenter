161f9ae0a0e6717f5c95f71e631b5f2c7f5fb2c4
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-03 10:51:56 -0700

    test student sign-up via invitation

--- spec/features/students_pages_spec.rb
+++ spec/features/students_pages_spec.rb
@@ -1,23 +1,25 @@
-feature 'Student signs up' do
+feature 'Student signs up via invitation' do
   before do
-    @plan = FactoryGirl.create :recurring_plan_with_upfront_payment
-    @cohort = FactoryGirl.create :future_cohort
-    visit new_student_registration_path
-    fill_in 'Email', with: 'example_user@example.com'
+    @student = FactoryGirl.create(:student)
+    @student.invite!
+    visit accept_student_invitation_path(@student, invitation_token: @student.raw_invitation_token)
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
   end
 
   scenario 'with valid information', js: true do
     fill_in 'Name', with: 'Ryan Larson'
-    select @plan.name, from: 'student_plan_id'
-    select @cohort.description, from: 'student_cohort_id'
-    click_on 'Sign up'
-    expect(page).to have_content 'How would you like to make payments'
+    select @student.plan.name, from: 'student_plan_id'
+    select @student.cohort.description, from: 'student_cohort_id'
+    click_on 'Submit'
+    expect(page).to have_content 'Your password was set successfully. You are now signed in.'
   end
 
   scenario 'with missing information', js: true do
-    click_on 'Sign up'
+    fill_in 'Name', with: ''
+    select @student.plan.name, from: 'student_plan_id'
+    select @student.cohort.description, from: 'student_cohort_id'
+    click_on 'Submit'
     expect(page).to have_content 'error'
   end
 end
