be1afbc28dc919854723921a8354710c89048895
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-06 15:00:51 -0700

    comment the after_invite_path method in the invitations controller as it overrides default behavior

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,7 @@
 class InvitationsController < Devise::InvitationsController
 
+#After an invitation is created and sent, the inviter will be redirected to this path (rather than the
+#after_sign_in_path_for within the Application Controller).
   def after_invite_path_for(user)
     root_path
   end
