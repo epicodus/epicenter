ac6c3df5d85122cbcd900465fed249fd7e28dc64
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-06 14:52:40 -0700

    remove invitable from an admin as admin inherits from user

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < User
   belongs_to :current_cohort, class_name: 'Cohort'
 
   before_create :assign_current_cohort
-  devise :database_authenticatable, :validatable, :invitable
+  devise :database_authenticatable, :validatable
   include DeviseInvitable::Inviter
 
 private
