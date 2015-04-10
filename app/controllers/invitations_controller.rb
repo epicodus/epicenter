539774dd38269bf7a6e9cf4633fc0b03f687b4d1
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-10 10:15:02 -0700

    call generated mailer instead of default mailer

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,17 @@
 class InvitationsController < Devise::InvitationsController
 
+  def create
+    @invited_user = User.invite!(invite_params, current_inviter) do |u|
+      # Skip sending the default Devise Invitable e-mail
+      u.skip_invitation = true
+    end
+
+    # Set the value for :invitation_sent_at because we skip calling the Devise Invitable method deliver_invitation which normally sets this value
+    @invited_user.update_attribute :invitation_sent_at, Time.now.utc unless @invited_user.invitation_sent_at
+    # Use our own mailer to send the invitation e-mail
+    InvitableMailer.invite_email(@invited_user, current_user).deliver
+  end
+
 #After an invitation is created and sent, the inviter will be redirected to this path (rather than the
 #after_sign_in_path_for within the Application Controller).
   def after_invite_path_for(user)
