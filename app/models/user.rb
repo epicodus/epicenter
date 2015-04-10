4a95080bcb86d6f01923fb555cb71e31c8c8f977
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-10 09:39:05 -0700

    send different invites to different user types

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,27 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
+
+  attr_writer :invitation_instructions
+
+  def deliver_invitation
+    if @invitation_instructions.present?
+      ::Devise.mailer.send(@invitation_instructions, self).deliver
+    else
+      super
+    end
+  end
+
+  def self.invite_student!(attributes={}, invited_by=nil)
+    self.invite!(attributes, invited_by) do |invitable|
+      invitable.invitation_instructions = :student_invitation_instructions
+    end
+  end
+
+  def self.invite_admin!(attributes={}, invited_by=nil)
+    self.invite!(attributes, invited_by) do |invitable|
+      invitable.invitation_instructions = :admin_invitation_instructions
+    end
+  end
+
 end
