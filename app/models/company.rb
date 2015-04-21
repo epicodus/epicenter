b72fca01da191d3abec271972bdb3dc9024692e5
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-21 16:08:54 -0700

    strip whitespace and raise exceptions for url input

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,10 +13,16 @@ class Company < ActiveRecord::Base
 private
 
   def fix_url
+    self.website = self.website.rstrip.lstrip
     if self.website
-      uri = URI.parse(self.website)
-      unless uri.scheme
-        self.website = URI::HTTP.build({ host: self.website }).to_s
+      begin
+        uri = URI.parse(self.website)
+        unless uri.scheme
+            self.website = URI::HTTP.build({ host: self.website }).to_s
+        end
+      rescue URI::InvalidURIError
+        errors.add(:website, "is invalid.")
+        false
       end
     end
   end
