106a42def4a1cfca4dd00a3d95d57ffa098f7ba7
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-21 21:21:50 -0700

    include update in attendance record routing

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :payments, only: [:index]
   resources :upfront_payments, only: [:create]
   resources :recurring_payments, only: [:create]
-  resources :attendance_records, only: [:create, :destroy]
+  resources :attendance_records, only: [:create, :update, :destroy]
   resources :attendance_record_amendments, only: [:new, :create]
   resources :cohorts, except: [:show, :index] do
     resources :attendance_statistics, only: [:index]
