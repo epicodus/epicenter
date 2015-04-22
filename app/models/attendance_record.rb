633ff5cca60631fee0c46b683074b1816d0c809c
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-21 20:13:35 -0700

    set tardy and left early upon signing in and check if left early by comparison to class end time

--- app/models/attendance_record.rb
+++ app/models/attendance_record.rb
@@ -4,16 +4,27 @@ class AttendanceRecord < ActiveRecord::Base
   validates :date, presence: true
 
   before_validation :set_date_and_tardiness
+  before_update :check_left_early
   belongs_to :student
 
 private
 
-  def set_tardiness
+  def sign_in
     if self.tardy.nil?
       class_late_time = Time.zone.parse(ENV['CLASS_START_TIME'] ||= '9:05 AM')
       current_time = Time.zone.now
       self.tardy = current_time >= class_late_time
+      self.left_early = true
     end
+  end
+
+  def check_left_early
+    class_end_time = Time.zone.parse(ENV['CLASS_END_TIME'] ||= '4:30 PM')
+    current_time = Time.zone.now
+    if current_time >= class_end_time
+      self.left_early = false
+    end
+    self.signed_out_time = current_time
   end
 
   def set_date
@@ -21,7 +32,7 @@ private
   end
 
   def set_date_and_tardiness
-    set_tardiness
+    sign_in
     set_date
   end
 end
