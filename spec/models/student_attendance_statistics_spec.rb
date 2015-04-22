d3358913bbe0cdf2d7389c9a2060bcc88b1dcd2f
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-22 15:58:48 -0700

    call save on signed out attendance record

--- spec/models/student_attendance_statistics_spec.rb
+++ spec/models/student_attendance_statistics_spec.rb
@@ -62,6 +62,7 @@ describe StudentAttendanceStatistics do
         attendance_record = FactoryGirl.create(:attendance_record, student: student)
         travel 7.hours do
           attendance_record.sign_out
+          attendance_record.save
           student_attendance_statistics = StudentAttendanceStatistics.new(student)
           expect(student_attendance_statistics.left_earlies).to eq [day_one.to_date]
         end
