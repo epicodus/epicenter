bb1d0aa6acbf370d78077980ba051a0d1d28334b
Jennifer McCarthy <jennifernward@gmail.com>
2015-04-30 02:05:44 +0000

    populate date attribute with Time.zone.now rather than Date.today

--- spec/models/attendance_record_amendment_spec.rb
+++ spec/models/attendance_record_amendment_spec.rb
@@ -7,45 +7,45 @@ describe AttendanceRecordAmendment do
     let(:student) { FactoryGirl.create(:student) }
 
     it 'creates a new attendance record for the student if they did not have one for the given date' do
-      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'On time')
+      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Time.zone.now.to_date, status: 'On time')
       attendance_record_amendment.save
-      expect(student.attendance_records.first.date).to eq(Date.today)
+      expect(student.attendance_records.first.date).to eq(Time.zone.now.to_date)
     end
 
     it 'sets tardy to false if the status is "On time"' do
-      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'On time')
+      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Time.zone.now.to_date, status: 'On time')
       attendance_record_amendment.save
       expect(student.attendance_records.first.tardy).to eq(false)
     end
 
     it 'sets left_early to false if the status is "On time"' do
-      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'On time')
+      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Time.zone.now.to_date, status: 'On time')
       attendance_record_amendment.save
       expect(student.attendance_records.first.left_early).to eq(false)
     end
 
     it 'sets tardy to true if the status is "Tardy"' do
-      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'Tardy')
+      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Time.zone.now.to_date, status: 'Tardy')
       attendance_record_amendment.save
       expect(student.attendance_records.first.tardy).to eq(true)
     end
 
     it 'sets left_early to true if the status is "Left early"' do
-      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'Left early')
+      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Time.zone.now.to_date, status: 'Left early')
       attendance_record_amendment.save
       expect(student.attendance_records.first.left_early).to eq(true)
     end
 
     it 'updates the status if an attendance record alread exists for the given day' do
-      FactoryGirl.create(:attendance_record, student: student, date: Date.today, tardy: true)
-      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'On time')
+      FactoryGirl.create(:attendance_record, student: student, date: Time.zone.now.to_date, tardy: true)
+      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Time.zone.now.to_date, status: 'On time')
       attendance_record_amendment.save
       expect(student.tardies).to eq 0
     end
 
     it 'destroys an existing attendance record for the given date if the status is "Absent"' do
-      FactoryGirl.create(:attendance_record, student: student, date: Date.today)
-      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Date.today, status: 'Absent')
+      FactoryGirl.create(:attendance_record, student: student, date: Time.zone.now.to_date)
+      attendance_record_amendment = AttendanceRecordAmendment.new(student_id: student.id, date: Time.zone.now.to_date, status: 'Absent')
       attendance_record_amendment.save
       expect(student.attendance_records.count).to eq 0
     end
