f42033126a9b978a62d98750cb10700deb49fb93
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-04 16:09:08 -0700

    no longer utilize account uri

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,7 +1,6 @@
 class PaymentMethod < ActiveRecord::Base
   scope :not_verified_first, -> { order(verified: :desc) }
 
-  # validates :account_uri, presence: true
   validates :student_id, presence: true
 
   belongs_to :student
