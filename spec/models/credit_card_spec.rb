8d176c9600d2450762e372250635ad9ea00f0e1c
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-08 14:32:28 -0700

    test creation of stripe card and retrieval of last four digits

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -1,17 +1,26 @@
 describe CreditCard do
   it { should belong_to :student }
   it { should have_many :payments }
-  # it { should validate_presence_of :account_uri }
   it { should validate_presence_of :student_id }
 
-  describe "create create card", :vcr do
+  describe "create Stripe card" do
     context "with a valid number" do
       let(:credit_card) { FactoryGirl.create :credit_card }
 
-      it "sets verified to 'true' and gets last four digits before_create" do
+      it "creates a Stripe card for a Stripe customer" do
         credit_card = FactoryGirl.create(:credit_card)
-        expect(credit_card.last_four_string).to eq "xxxxxxxxxxxx1111"
+        stripe_card = credit_card.student.stripe_customer.sources.all.first
+        expect(stripe_card).to be_an_instance_of(Stripe::Card)
+      end
+
+      it "sets verified to 'true' before_create" do
+        credit_card = FactoryGirl.create(:credit_card)
         expect(credit_card.verified).to eq true
+      end
+
+      it "gets last four digits after_create" do
+        credit_card = FactoryGirl.create(:credit_card)
+        expect(credit_card.last_four_string).to eq "4242"
       end
 
       it "is made the primary payment method if student does not have one" do
@@ -21,29 +30,22 @@ describe CreditCard do
     end
   end
 
-  describe "#fetch_balanced_account" do
-    it "returns the balanced credit card object", :vcr do
-      credit_card = FactoryGirl.create :credit_card
-      expect(credit_card.fetch_balanced_account.href).to eq credit_card.account_uri
-    end
-  end
-
   describe "#starting_status" do
-    it "returns 'succeeded'", :vcr do
+    it "returns 'succeeded'" do
       credit_card = FactoryGirl.create :credit_card
       expect(credit_card.starting_status).to eq 'succeeded'
     end
   end
 
   describe "#calculate_fee" do
-    it "returns the credit card fees for the amount given", :vcr do
+    it "returns the credit card fees for the amount given" do
       credit_card = FactoryGirl.create :credit_card
       expect(credit_card.calculate_fee(600_00)).to eq 18_21
     end
   end
 
   describe "#verified?" do
-    it "returns true", :vcr do
+    it "returns true" do
       credit_card = FactoryGirl.create :credit_card
       expect(credit_card.verified?).to eq true
     end
