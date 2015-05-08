e7c59794576a0cd0e997af551b7063bce89b2599
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-08 14:43:05 -0700

    no longer verify presence of account uri on a payment method

--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -1,5 +1,4 @@
 describe PaymentMethod do
-  it { should validate_presence_of :account_uri }
   it { should validate_presence_of :student_id }
   it { should belong_to :student }
   it { should have_many :payments }
