89b72c01c4b7d873d6b2fb270136a08da5fed346
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-08 14:38:58 -0700

    reflect changes made to the user model for a stripe customer id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429231911) do
+ActiveRecord::Schema.define(version: 20150505232054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,6 +181,7 @@ ActiveRecord::Schema.define(version: 20150429231911) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type",           limit: 255
     t.integer  "invitations_count",                     default: 0
+    t.string   "stripe_customer_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
