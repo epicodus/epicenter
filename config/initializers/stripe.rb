e68c311647b9bb24574b325f12e01433e92afebd
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-08 14:36:03 -0700

    set up test and production env with Stripe ENV keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,13 @@
+require 'stripe'
+
 if Rails.env == 'production'
   Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLIC_KEY']
+    :publishable_key => ENV['STRIPE_PUBLIC_KEY'],
     :secret_key => ENV['STRIPE_API_KEY']
   }
 else
   Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_TEST_PUBLIC_KEY']
+    :publishable_key => ENV['STRIPE_TEST_PUBLIC_KEY'],
     :secret_key => ENV['STRIPE_TEST_API_KEY']
   }
 end
