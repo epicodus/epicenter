d899a1d5cf9677692a1400f6800be445c65b2410
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-08 14:19:21 -0700

    add restful actions to edit and update an existing bank account with test deposit amounts now permitted in params

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -12,12 +12,21 @@ class BankAccountsController < ApplicationController
     end
   end
 
-private
-
-  def bank_account_params
-    params.require(:bank_account).permit(:account_uri, :verification_uri)
+  def edit
+    @bank_account = BankAccount.find(params[:bank_account_id])
   end
-  # def bank_account_params
-  #   params.require(:bank_account).permit(:stripe_token)
-  # end
+
+  def update
+    @bank_account = BankAccount.find(params[:bank_account_id])
+    if @bank_account.update(bank_account_params)
+      redirect_to payment_methods_path, notice: "Your bank account has been confirmed."
+    else
+      render :edit
+    end
+  end
+
+private
+  def bank_account_params
+    params.require(:bank_account).permit(:first_depost, :second_deposit, :stripe_token)
+  end
 end
