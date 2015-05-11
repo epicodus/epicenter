a2bfe27c5361e14093e0a7e04ac87c4825c5a0a0
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-11 16:18:03 -0700

    delete all student vcr cassettes and rerun with Stripe information

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -28,17 +28,17 @@ describe Student do
   end
 
   describe "#stripe_customer" do
-    it "creates a Stripe Customer object for a student" do
+    it "creates a Stripe Customer object for a student", :vcr do
       student = FactoryGirl.create(:student)
       expect(student.stripe_customer).to be_an_instance_of(Stripe::Customer)
     end
 
-    it "returns the Stripe Customer object" do
+    it "returns the Stripe Customer object", :vcr do
       student = FactoryGirl.create(:student)
       expect(student.stripe_customer).to be_an_instance_of(Stripe::Customer)
     end
 
-    it "returns a Stripe Customer object if one already exists" do
+    it "returns a Stripe Customer object if one already exists", :vcr do
       student = FactoryGirl.create(:student)
       first_stripe_customer_return = student.stripe_customer
       second_stripe_customer_return = student.stripe_customer
@@ -47,12 +47,12 @@ describe Student do
   end
 
   describe "#stripe_customer_id" do
-    it "starts out nil" do
+    it "starts out nil", :vcr do
       student = FactoryGirl.create(:student)
       expect(student.stripe_customer_id).to be_nil
     end
 
-    it "is populated when a Stripe Customer object is created" do
+    it "is populated when a Stripe Customer object is created", :vcr do
       student = FactoryGirl.create(:student)
       stripe_customer = student.stripe_customer
       expect(student.stripe_customer_id).to eq stripe_customer.id
@@ -429,15 +429,6 @@ describe Student do
 
       it { is_expected.to have_abilities(:read, Payment.new(student: student)) }
       it { is_expected.to not_have_abilities(:read, Payment.new) }
-    end
-
-    context 'for verifications', vcr: true do
-      it 'allows students to verify their own bank accounts' do
-        bank_account = FactoryGirl.create(:bank_account, student: student)
-        is_expected.to have_abilities(:update, Verification.new(bank_account: bank_account))
-      end
-
-      it { is_expected.to not_have_abilities(:update, Verification.new) }
     end
 
     context 'for companies' do
