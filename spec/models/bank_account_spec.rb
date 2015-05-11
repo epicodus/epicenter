37463ac3f556f5e118b29fbad4a3edbbb926c6b0
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-11 14:10:29 -0700

    turn on vcr to record Stripe API call for bank accounts

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -3,7 +3,7 @@ describe BankAccount do
   it { should belong_to :student }
   it { should have_many :payments }
 
-  describe "create bank account" do
+  describe "create bank account", :vcr do
     let(:bank_account) { FactoryGirl.create :bank_account }
 
     it "creates a Stripe bank account" do
@@ -18,7 +18,7 @@ describe BankAccount do
     end
   end
 
-  describe "verify Stripe account" do
+  describe "verify Stripe account", :vcr do
     let(:student) { FactoryGirl.create :user_with_verified_bank_account }
 
     it "verifies a Stripe account after_update" do
@@ -30,26 +30,26 @@ describe BankAccount do
   end
 
   describe "#create_verification" do
-    it "sets the student's bank_account verified status to true" do
+    it "sets the student's bank_account verified status to true", :vcr do
       student = FactoryGirl.create(:user_with_verified_bank_account)
       expect(student.bank_accounts.first.verified).to be true
     end
 
-    it "sets the confirmed bank account as the primary payment method if student does not have one" do
+    it "sets the confirmed bank account as the primary payment method if student does not have one", :vcr do
       student = FactoryGirl.create(:user_with_verified_bank_account)
       expect(student.primary_payment_method).to be_an_instance_of(BankAccount)
     end
   end
 
   describe "#starting_status" do
-    it "returns 'pending'" do
+    it "returns 'pending'", :vcr do
       bank_account = FactoryGirl.create :bank_account
       expect(bank_account.starting_status).to eq 'pending'
     end
   end
-  
+
   describe "#calculate_fee" do
-    it "returns zero" do
+    it "returns zero", :vcr do
       bank_account = FactoryGirl.create :verified_bank_account
       expect(bank_account.calculate_fee(600_00)).to eq 0
     end
