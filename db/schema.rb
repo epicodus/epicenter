4fdf87ebb1d6ed56837ced4ee287bcff8d132a69
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-22 10:36:17 -0700

    populate payment methods table with stripe id from bank_account or card

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520233623) do
+ActiveRecord::Schema.define(version: 20150522171449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20150520233623) do
     t.string   "type",             limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "stripe_id"
   end
 
   create_table "payments", force: :cascade do |t|
