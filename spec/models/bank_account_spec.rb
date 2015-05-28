865179022ec90a595c38ba1ab67c5aa928d6d7b1
Jennifer McCarthy <jennifernward@gmail.com>
2015-05-28 13:56:11 -0700

    include proper js and vcr references for testing bank accounts

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -3,31 +3,31 @@ describe BankAccount do
   it { should belong_to :student }
   it { should have_many :payments }
 
-  describe "create bank account", :vcr do
+  describe "create bank account", :vcr, js: true do
     let(:bank_account) { FactoryGirl.create :bank_account }
 
-    it "creates a Stripe bank account", :vcr do
+    it "creates a Stripe bank account", :vcr, js: true do
       account = FactoryGirl.create(:bank_account)
       stripe_account = account.student.stripe_customer.bank_accounts.first
       expect(stripe_account).to be_an_instance_of(Stripe::BankAccount)
     end
 
-    it "sets the stripe_id for the bank account instance", :vcr do
+    it "sets the stripe_id for the bank account instance", :vcr, js: true do
       account = FactoryGirl.create(:bank_account)
       stripe_account = account.student.stripe_customer.bank_accounts.first
       expect(account.stripe_id).to eq stripe_account.id
     end
 
-    it "gets last four digits before_create", :vcr do
+    it "gets last four digits before_create", :vcr, js: true do
       bank_account = FactoryGirl.create(:bank_account)
       expect(bank_account.last_four_string).to eq "6789"
     end
   end
 
-  describe "verify Stripe account", :vcr do
+  describe "verify Stripe account", :vcr, js: true do
     let(:student) { FactoryGirl.create :user_with_verified_bank_account }
 
-    it "verifies a Stripe account before_update", :vcr do
+    it "verifies a Stripe account before_update", :vcr, js: true do
       bank_account = FactoryGirl.create(:bank_account)
       bank_account.update(first_deposit: 32, second_deposit: 45)
       stripe_account = bank_account.student.stripe_customer.bank_accounts.retrieve(bank_account.stripe_id)
@@ -35,27 +35,27 @@ describe BankAccount do
     end
   end
 
-  describe "#verify_account", :vcr do
-    it "sets the student's bank_account verified status to true", :vcr do
+  describe "#verify_account", :vcr, js: true do
+    it "sets the student's bank_account verified status to true", :vcr, js: true do
       student = FactoryGirl.create(:user_with_verified_bank_account)
       expect(student.bank_accounts.first.verified).to be true
     end
 
-    it "sets the confirmed bank account as the primary payment method if student does not have one", :vcr do
+    it "sets the confirmed bank account as the primary payment method if student does not have one", :vcr, js: true do
       student = FactoryGirl.create(:user_with_verified_bank_account)
       expect(student.primary_payment_method).to be_an_instance_of(BankAccount)
     end
   end
 
   describe "#starting_status" do
-    it "returns 'pending'", :vcr do
+    it "returns 'pending'", :vcr, js: true do
       bank_account = FactoryGirl.create :bank_account
       expect(bank_account.starting_status).to eq 'pending'
     end
   end
 
   describe "#calculate_fee" do
-    it "returns zero", :vcr do
+    it "returns zero", :vcr, js: true do
       bank_account = FactoryGirl.create :verified_bank_account
       expect(bank_account.calculate_fee(600_00)).to eq 0
     end
