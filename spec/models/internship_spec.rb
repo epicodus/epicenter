49ef5b2a3a76792be41dbf894b6ed742e283fa17
Jake Kaad <kaadalac@gmail.com>
2015-06-05 15:41:56 -0700

    add by_company_name scope to internships

--- spec/models/internship_spec.rb
+++ spec/models/internship_spec.rb
@@ -8,18 +8,4 @@ describe Internship do
   it { should validate_presence_of :description }
   it { should validate_presence_of :ideal_intern }
   it { should validate_uniqueness_of(:company_id).scoped_to(:cohort_id) }
-
-  describe 'default scope' do
-    let(:company) { FactoryGirl.create(:company, name: "z labs") }
-    let!(:company_two) { FactoryGirl.create(:company, name: "a labs") }
-    let!(:company_three) { FactoryGirl.create(:company, name: 'k labs') }
-
-    let!(:internship) { FactoryGirl.create(:internship, company: company) }
-    let!(:internship_two) { FactoryGirl.create(:internship, company: company_two) }
-    let!(:internship_three) { FactoryGirl.create(:internship, company: company_three) }
-
-    it 'should be organized alphabetically by company name' do
-      expect(Internship.all).to eq [internship_two, internship_three, internship]
-    end
-  end
 end
