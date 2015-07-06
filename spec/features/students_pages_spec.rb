eda804ee123e74a77a4201bfd944d864c5301083
Christopher Joy-Webb <chris@joywebb.com>
2015-07-06 15:18:58 -0700

    add vcr to an updated student integration test that wasn't passing when offline

--- spec/features/students_pages_spec.rb
+++ spec/features/students_pages_spec.rb
@@ -42,7 +42,7 @@ feature "Student signs in while class is not in session" do
   let(:future_cohort) { FactoryGirl.create(:future_cohort) }
   let(:student) { FactoryGirl.create(:student, cohort: future_cohort) }
 
-  context "before adding a payment method" do
+  context "before adding a payment method", :vcr do
     it "takes them to the page to choose payment method" do
       sign_in(student)
       visit new_payment_method_path
