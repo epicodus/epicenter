9bfcce02f049bee7e47598f92377fb1a3fd03d4e
Christopher Joy-Webb <chris@joywebb.com>
2015-07-08 14:49:07 -0700

    add flash alert when student visits the url of a document he or she has already signed

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -7,6 +7,7 @@ class SignaturesController < ApplicationController
     update_signature_request
     if current_student.signed?(signature_model)
       redirect_to root_path
+      flash[:alert] = "You've already signed this document."
     elsif current_user.signed?(signed_signature_model)
       signature = signature_model.create(student_id: current_student.id)
       @sign_url = signature.sign_url
