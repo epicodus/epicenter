153cc7395edbd00f472d5e0042674a3d14fc5d4b
Christopher Joy-Webb <chris@joywebb.com>
2015-07-09 14:50:31 -0700

    update enrollment agreement to redirect to correct path

--- app/controllers/enrollment_agreement_controller.rb
+++ app/controllers/enrollment_agreement_controller.rb
@@ -3,6 +3,6 @@ class EnrollmentAgreementController < SignaturesController
   before_filter :authenticate_student!
 
   def new
-    super(RefundPolicy, EnrollmentAgreement, 'recurring_payments_option', :index)
+    super(RefundPolicy, EnrollmentAgreement, 'payment_options', :new)
   end
 end
