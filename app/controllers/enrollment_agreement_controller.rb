6b53651d8d72a2ca06e1a201e99006220dc95357
Christopher Joy-Webb <chris@joywebb.com>
2015-07-10 10:15:58 -0700

    update enrollment agreement controller to check if student has a recurring plan and redirect accordingly

--- app/controllers/enrollment_agreement_controller.rb
+++ app/controllers/enrollment_agreement_controller.rb
@@ -3,6 +3,10 @@ class EnrollmentAgreementController < SignaturesController
   before_filter :authenticate_student!
 
   def new
-    super(RefundPolicy, EnrollmentAgreement, 'payment_options', :new)
+    if current_user.plan.recurring?
+      super(RefundPolicy, EnrollmentAgreement, 'promissory_note', :new)
+    else
+      super(RefundPolicy, EnrollmentAgreement, 'payment_methods', :new)
+    end
   end
 end
