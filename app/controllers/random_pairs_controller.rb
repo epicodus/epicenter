39d21c889ccdf60b8caf272c8e86635cd458b1e5
Christopher Joy-Webb <chris@joywebb.com>
2015-08-29 14:36:37 -0700

    update random pairs controller action to redirect to root when it's the weekend

--- app/controllers/random_pairs_controller.rb
+++ app/controllers/random_pairs_controller.rb
@@ -1,9 +1,15 @@
 class RandomPairsController < ApplicationController
 
+  include WeekdayHelper
+
   before_filter :authenticate_student!
 
   def show
-    @grade_students = current_student.similar_grade_students
-    @random_pair = @grade_students.sample(rand(@grade_students.count)).first
+    if is_weekday?
+      @grade_students = current_student.similar_grade_students
+      @random_pair = @grade_students.sample(rand(@grade_students.count)).first
+    else
+      redirect_to root_path
+    end
   end
 end
