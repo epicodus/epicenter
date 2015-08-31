2b388a20d4f4005d99060cdf0d85f855b4b44bdd
Christopher Joy-Webb <chris@joywebb.com>
2015-08-31 10:07:40 -0700

    update random pair integration tests to use native Rails time travel instead of timecop

--- spec/features/random_pair_pages_spec.rb
+++ spec/features/random_pair_pages_spec.rb
@@ -1,6 +1,9 @@
 feature 'viewing the random pair page' do
   let(:student) { FactoryGirl.create(:user_with_all_documents_signed) }
   let(:admin) { FactoryGirl.create(:admin) }
+  let(:monday) { Time.zone.now.to_date.beginning_of_week }
+  let(:friday) { monday + 4.days }
+  let(:saturday) { monday + 5.days }
 
   scenario 'viewing the random pair page as a guest' do
     visit random_pair_path
@@ -13,19 +16,19 @@ feature 'viewing the random pair page' do
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 
-  scenario 'viewing the random pair page on a weekend' do
+  scenario 'viewing the random pair page on a weekend as a current student' do
     login_as(student, scope: :student)
-    today = Time.local(2015, 8, 29, 12, 0, 0)
-    Timecop.freeze(today)
-    visit random_pair_path
-    expect(current_path).to eql cohort_code_reviews_path(student.cohort)
+    travel_to saturday do
+      visit random_pair_path
+      expect(current_path).to eql cohort_code_reviews_path(student.cohort)
+    end
   end
 
   scenario 'viewing the random pair page on a weekday' do
     login_as(student, scope: :student)
-    today = Time.local(2015, 8, 28, 12, 0, 0)
-    Timecop.freeze(today)
-    visit random_pair_path
-    expect(current_path).to eql random_pair_path
+    travel_to friday do
+      visit random_pair_path
+      expect(current_path).to eql random_pair_path
+    end
   end
 end
