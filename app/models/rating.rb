f957d88714ce966a39af1e7a56a8ae55152c2814
Christopher Joy-Webb <chris@joywebb.com>
2015-09-02 13:30:06 -0700

    add Rating model validation for low rated internships

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,7 @@
   belongs_to :internship
 
   validates :internship_id, presence: true, uniqueness: { scope: :student_id }
+  validate :no_more_than_five_lowest
 
   def self.for(internship, current_student)
     if !current_student
@@ -11,6 +12,15 @@
       current_student.find_rating(internship)
     else
       current_student.ratings.new
+    end
+  end
+
+private
+
+  def no_more_than_five_lowest
+    if interest == "3" && student.ratings.where(interest: 3).count == 5
+      errors.add(:interest, "cannot be low for more than 5 companies.")
+      false
     end
   end
 end
