ad165cdf5256ed5b8e68d8b4f4ecae6e7aa15eb4
Christopher Joy-Webb <chris@joywebb.com>
2015-09-02 13:31:59 -0700

    update ratings controller to integrate with new ratings form

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,15 +1,20 @@
 class RatingsController < ApplicationController
 
   def create
-    rating = Rating.for(Internship.find(params[:internship_id]), current_student)
-    rating.update(rating_params)
-    redirect_to :back
+    @rating = Rating.for(Internship.find(params[:internship_id]), current_student)
+    if @rating.update(rating_params)
+      redirect_to cohort_internships_path(current_student.cohort)
+    else
+      @cohort = current_student.cohort
+      @internships = @cohort.internships_sorted_by_interest(current_student)
+      render 'internships/index'
+    end
   end
 
 
   private
 
   def rating_params
-    { interest: params[:commit].keys.first.to_i, internship_id: params[:internship_id], notes: params[:notes] }
+    { interest: params[:rating][:interest].to_i, internship_id: params[:internship_id], notes: params[:notes] }
   end
 end
