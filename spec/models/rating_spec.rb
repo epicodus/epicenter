62d97d256ebaee88985ea58939150e95f84490c9
Christopher Joy-Webb <chris@joywebb.com>
2015-09-03 09:44:20 -0700

    add passing tests for no_more_than_five_lowest validation for rating model

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -3,6 +3,25 @@ describe Rating do
   it { should belong_to :student }
   it { should validate_uniqueness_of(:internship_id).scoped_to(:student_id) }
 
+  describe '#no_more_than_five_lowest validation' do
+    let(:student) { FactoryGirl.create(:student) }
+
+    before do
+      FactoryGirl.create_list(:rating, 3, student: student, interest: 3)
+    end
+
+    it 'marks an internship as low priority if a student has <= 5 low priority internships' do
+      low_rating = FactoryGirl.build(:rating, student: student, interest: 3)
+      expect(low_rating.save).to be true
+    end
+
+    it 'does not create a "Low" rating when a student already has 5 "Low" ratings' do
+      FactoryGirl.create_list(:rating, 2, student: student, interest: 3)
+      low_rating = Rating.new(student: student, interest: 3, internship_id: 1)
+      expect(low_rating.save).to be false
+    end
+  end
+
   describe 'for' do
     let(:student) { FactoryGirl.create(:student) }
     let(:internship) { FactoryGirl.create(:internship, cohort: student.cohort) }
