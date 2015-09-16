1ce4dc163a7f398a1cf71fae937ed47563f39b17
Christopher Joy-Webb <chris@joywebb.com>
2015-09-16 16:43:09 -0700

    revert to original attendance records controller

--- app/controllers/attendance_records_controller.rb
+++ app/controllers/attendance_records_controller.rb
@@ -7,23 +7,15 @@ class AttendanceRecordsController < ApplicationController
   end
 
   def create
-    attendance_records = params[:pair_ids].map do |pair_id|
-      { student_id: pair_id, pair_id: (params[:pair_ids] - [pair_id]).first }
-    end
-    if params[:pair_ids].uniq.count > 1 || params[:pair_ids].count == 1
-      if @attendance_records = AttendanceRecord.create(attendance_records)
-        student_names = @attendance_records.map { |attendance_record| attendance_record.student.name }
-        flash[:notice] = "Welcome #{student_names.join(' and ')}."
-        flash[:secure] =  view_context.link_to("Wrong student?",
-                    destroy_multiple_attendance_records_path(ids: @attendance_records.map(&:id)),
-                    data: {method: :delete})
-        redirect_to attendance_path
-      else
-        flash[:alert] = "Something went wrong: " + @attendance_records.first.errors.full_messages.join(", ")
-        redirect_to attendance_path
-      end
+    @attendance_record = AttendanceRecord.new(attendance_record_params)
+    if @attendance_record.save
+      flash[:notice] = "Welcome #{@attendance_record.student.name}"
+      flash[:secure] =  view_context.link_to("Not you?",
+                        attendance_record_path(@attendance_record),
+                        data: {method: :delete})
+      redirect_to attendance_path
     else
-      flash[:alert] = "Cannot add the same student twice."
+      flash[:alert] = "Something went wrong: " + @attendance_record.errors.full_messages.join(", ")
       redirect_to attendance_path
     end
   end
@@ -38,15 +30,15 @@ class AttendanceRecordsController < ApplicationController
     end
   end
 
-  def destroy_multiple
-    AttendanceRecord.destroy(params[:ids])
-    flash[:alert] = params[:ids].count == 1 ? "Attendance record has been deleted." : "Attendance records have been deleted."
-    redirect_to attendance_path
+  def destroy
+    @attendance_record = AttendanceRecord.find(params[:id])
+    @attendance_record.destroy
+    redirect_to attendance_path, alert: "Attendance record has been deleted."
   end
 
 private
 
   def attendance_record_params
-    params.require(:attendance_record).permit(:signing_out, :student_id, :pair_ids)
+    params.require(:attendance_record).permit(:signing_out, :student_id)
   end
 end
