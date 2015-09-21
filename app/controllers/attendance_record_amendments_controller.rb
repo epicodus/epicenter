68f806467ef93d2fc2dfe9f1d338aec69ba38506
Christopher Joy-Webb <chris@joywebb.com>
2015-09-21 16:13:42 -0700

    refactor attendance record amendments create action to remove double save and include check_referring_url private method; update formatted_class_days in attendance helper

--- app/controllers/attendance_record_amendments_controller.rb
+++ app/controllers/attendance_record_amendments_controller.rb
@@ -8,14 +8,10 @@ class AttendanceRecordAmendmentsController < ApplicationController
   end
 
   def create
-    @attendance_record_amendment = AttendanceRecordAmendment.new(attendance_record_amendment_params)
-    if @attendance_record_amendment.save && request.referer.include?('attendance_statistics')
+    attendance_record_amendment = AttendanceRecordAmendment.new(attendance_record_amendment_params)
+    if attendance_record_amendment.save
       student = Student.find(params[:attendance_record_amendment][:student_id])
-      day = params[:attendance_record_amendment][:date]
-      redirect_to cohort_attendance_statistics_path(student.cohort, day: day), notice: "#{student.name}'s attendance record has been amended."
-    elsif @attendance_record_amendment.save
-      student = Student.find(params[:attendance_record_amendment][:student_id])
-      redirect_to student_path(student), notice: "#{student.name}'s attendance record has been amended."
+      check_referring_url(student)
     else
       render :new
     end
@@ -25,5 +21,15 @@ private
 
   def attendance_record_amendment_params
     params.require(:attendance_record_amendment).permit(:student_id, :status, :date)
+  end
+
+  def check_referring_url(student)
+    if request.referer.include?('attendance_statistics')
+      day = params[:attendance_record_amendment][:date]
+      redirect_to cohort_attendance_statistics_path(student.cohort, day: day), notice: "#{student.name}'s attendance record has been amended."
+    else
+      student = Student.find(params[:attendance_record_amendment][:student_id])
+      redirect_to student_path(student), notice: "#{student.name}'s attendance record has been amended."
+    end
   end
 end
--- app/helpers/attendance_helper.rb
+++ app/helpers/attendance_helper.rb
@@ -1,6 +1,6 @@
 module AttendanceHelper
   def formatted_class_days
-    @class_days.map { |day| [day.strftime("%B %d, %Y, %A"), day] }
+    @cohort.past_and_present_class_days.map { |day| [day.strftime("%B %d, %Y, %A"), day] }
   end
 
   def day_value
