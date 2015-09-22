d3d200e0147223c1d5e153e9638efd35667eb8d2
Christopher Joy-Webb <chris@joywebb.com>
2015-09-22 14:16:21 -0700

    add vcr to bank account integration tests because stripe is adding new cassettes randomly

--- spec/features/bank_accounts_pages_spec.rb
+++ spec/features/bank_accounts_pages_spec.rb
@@ -20,7 +20,7 @@ feature 'Creating a bank account' do
       expect(page).to have_content '2-3 business days'
     end
 
-    scenario 'with missing account number', js: true do
+    scenario 'with missing account number', :vcr, js: true do
       fill_in 'Routing number', with: '110000000'
       fill_in 'Bank account number', with: ' '
       click_on 'Verify bank account'
@@ -29,7 +29,7 @@ feature 'Creating a bank account' do
       end
     end
 
-    scenario 'with invalid routing number', js: true do
+    scenario 'with invalid routing number', :vcr, js: true do
       fill_in 'Bank account number', with: '000123456789'
       fill_in 'Routing number', with: '12345689'
       click_on 'Verify bank account'
