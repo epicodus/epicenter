831ceeac08c0ee0480dfc57d984e965624539bec
Christopher Joy-Webb <chris@joywebb.com>
2015-09-24 12:07:05 -0700

    update random pairs view to reflect updated random_pairs method; add random pairs integration test for viewing random pair names

--- spec/features/random_pair_pages_spec.rb
+++ spec/features/random_pair_pages_spec.rb
@@ -1,5 +1,7 @@
 feature 'viewing the random pair page' do
   let(:student) { FactoryGirl.create(:user_with_all_documents_signed) }
+  let(:student_2) { FactoryGirl.create(:student) }
+  let(:student_3) { FactoryGirl.create(:student) }
   let(:admin) { FactoryGirl.create(:admin) }
   let(:monday) { Time.zone.now.to_date.beginning_of_week }
   let(:friday) { monday + 4.days }
@@ -24,11 +26,20 @@ feature 'viewing the random pair page' do
     end
   end
 
-  scenario 'viewing the random pair page on a weekday' do
+  scenario 'visiting the random pair page on a weekday' do
     login_as(student, scope: :student)
     travel_to friday do
       visit random_pairs_path
       expect(current_path).to eql random_pairs_path
+    end
+  end
+
+  scenario 'viewing random pairs on a weekday' do
+    allow(student).to receive(:random_pairs).and_return [student_3, student_2]
+    login_as(student, scope: :student)
+    travel_to friday do
+      visit random_pairs_path
+      expect(page).to have_content student_2.name && student_3.name
     end
   end
 end
