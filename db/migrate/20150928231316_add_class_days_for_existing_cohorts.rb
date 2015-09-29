b42110ded21014126def901f6aa8df3c8680e532
Christopher Joy-Webb <chris@joywebb.com>
2015-09-29 11:41:08 -0700

    add migration that seeds class_days for all cohorts

--- db/migrate/20150928231316_add_class_days_for_existing_cohorts.rb
+++ db/migrate/20150928231316_add_class_days_for_existing_cohorts.rb
@@ -1,7 +1,7 @@
 class AddClassDaysForExistingCohorts < ActiveRecord::Migration
   def change
     Cohort.all.each do |cohort|
-      cohort.update(class_days: (cohort.start_date..cohort.end_date).to_a.select { |day| day.to_s if !day.friday? && !day.saturday? && !day.sunday? }.join(','))
+      cohort.update(class_days: (cohort.start_date..cohort.end_date).select { |day| day if !day.friday? && !day.saturday? && !day.sunday? })
     end
   end
 end
