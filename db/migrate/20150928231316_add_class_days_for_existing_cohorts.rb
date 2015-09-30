44f9a41a21c808d5a9f88457caa56f1d53b3b8da
Christopher Joy-Webb <chris@joywebb.com>
2015-09-30 14:15:39 -0700

    edit Cohort migration seeding to have proper rollback behavior

--- db/migrate/20150928231316_add_class_days_for_existing_cohorts.rb
+++ db/migrate/20150928231316_add_class_days_for_existing_cohorts.rb
@@ -1,7 +1,14 @@
 class AddClassDaysForExistingCohorts < ActiveRecord::Migration
-  def change
+  def up
     Cohort.all.each do |cohort|
       cohort.update(class_days: (cohort.start_date..cohort.end_date).select { |day| !day.friday? && !day.saturday? && !day.sunday? })
+    end
+  end
+
+  def down
+    Cohort.all.each do |cohort|
+      cohort.assign_attributes(class_days: nil)
+      cohort.save(validate: false)
     end
   end
 end
--- db/migrate/20150929234517_add_start_and_end_times_to_existing_cohorts.rb
+++ db/migrate/20150929234517_add_start_and_end_times_to_existing_cohorts.rb
@@ -1,6 +1,11 @@
 class AddStartAndEndTimesToExistingCohorts < ActiveRecord::Migration
-  def change
+  def up
     Cohort.where(description: ['Intro to Programming', 'Winter 2016 Intro to Programming PT']).update_all(start_time: '6:20 PM', end_time: '8:40 PM')
     Cohort.where.not(description: ['Intro to Programming', 'Winter 2016 Intro to Programming PT']).update_all(start_time: '9:20 AM', end_time: '4:30 PM')
+  end
+
+  def down
+    Cohort.where(description: ['Intro to Programming', 'Winter 2016 Intro to Programming PT']).update_all(start_time: nil, end_time: nil)
+    Cohort.where.not(description: ['Intro to Programming', 'Winter 2016 Intro to Programming PT']).update_all(start_time: nil, end_time: nil)
   end
 end
