3af6673c6cf34dd6c115635d2b37f07aec57d8d2
Christopher Joy-Webb <chris@joywebb.com>
2015-10-01 09:16:26 -0700

    update Cohort start/end times in migration seed and reorder migrations

--- db/migrate/20150929234517_add_start_and_end_times_to_existing_cohorts.rb
+++ db/migrate/20150929234517_add_start_and_end_times_to_existing_cohorts.rb
@@ -1,7 +1,7 @@
 class AddStartAndEndTimesToExistingCohorts < ActiveRecord::Migration
   def up
-    Cohort.where(description: ['Intro to Programming', 'Winter 2016 Intro to Programming PT']).update_all(start_time: '6:20 PM', end_time: '8:40 PM')
-    Cohort.where.not(description: ['Intro to Programming', 'Winter 2016 Intro to Programming PT']).update_all(start_time: '9:20 AM', end_time: '4:30 PM')
+    Cohort.where(description: ['Intro to Programming', 'Winter 2016 Intro to Programming PT']).update_all(start_time: '6:00 PM', end_time: '9:00 PM')
+    Cohort.where.not(description: ['Intro to Programming', 'Winter 2016 Intro to Programming PT']).update_all(start_time: '9:00 AM', end_time: '5:00 PM')
   end
 
   def down
