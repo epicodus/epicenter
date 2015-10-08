3d1c74b19534cba8b42e560e33e740e30436c55b
Christopher Joy-Webb <chris@joywebb.com>
2015-10-08 13:42:05 -0700

    upate students js scripts to work with new cohorts tab on student show view

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,17 +1,31 @@
 $(function() {
   $('.student-internships').hide();
+  $('.student-cohorts').hide();
 
   $('.student-internships-header').click(function() {
     $('.student-attendance').hide();
+    $('.student-cohorts').hide();
     $('.student-internships').show();
     $('.student-attendance-header').removeClass("active");
+    $('.student-cohorts-header').removeClass("active");
     $('.student-internships-header').addClass("active");
   });
 
   $('.student-attendance-header').click(function() {
     $('.student-internships').hide();
+    $('.student-cohorts').hide();
     $('.student-attendance').show();
     $('.student-attendance-header').addClass("active");
+    $('.student-internships-header').removeClass("active");
+    $('.student-cohorts-header').removeClass("active");
+  });
+
+  $('.student-cohorts-header').click(function() {
+    $('.student-internships').hide();
+    $('.student-attendance').hide();
+    $('.student-cohorts').show();
+    $('.student-cohorts-header').addClass("active");
+    $('.student-attendance-header').removeClass("active");
     $('.student-internships-header').removeClass("active");
   });
 });
