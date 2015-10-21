0673acaaf3a1d1b5bdc7ec812ddd7d6c046d356b
Christopher Joy-Webb <chris@joywebb.com>
2015-10-21 16:37:10 -0700

    update email sender to debbie@epicodus.com

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'students@epicodus.com'
+  config.mailer_sender = 'debbie@epicodus.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
