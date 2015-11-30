6e4887ed4b621c8f5826fa9b8278d7474cc5469f
Christopher Joy-Webb <chris@joywebb.com>
2015-11-30 13:41:18 -0800

    add custom js for updating multiple ratings and ratings fields_for helper

---
+++ app/assets/javascripts/ratings.js
@@ -0,0 +1,35 @@
+$(function() {
+  $('.rating-text-area').keyup(function() {
+    text_length = $(this).val().length;
+    $(this).parent().parent().find('p').html(text_length + ' characters');
+
+    if ($(this).val().length < 10) {
+      $(this).parent().addClass('has-error');
+      $('.fixed-ratings-button').attr('disabled', true);
+    } else if ($(this).val().length > 9) {
+      $(this).parent().removeClass('has-error');
+      $('.fixed-ratings-button').attr('disabled', false);
+    }
+
+    if (!$(this).val() && $(this).parent().hasClass('has-error')) {
+      $(this).parent().removeClass('has-error');
+      $('.fixed-ratings-button').attr('disabled', false);
+    }
+  });
+
+  $('input:radio[value=3]').change(function() {
+    if ($('input:radio:checked[value=3]').length > 5) {
+      $(this).attr('checked', false);
+      $(this).parent().removeClass('active');
+      $(this).tooltip('toggle');
+    }
+  });
+
+  $('input:text, textarea').change(function() {
+    $(this).parent().parent().find($('input:radio')).attr('required', true)
+  });
+
+  $('input:radio').change(function() {
+    $(this).parent().parent().parent().find($('input:text, textarea')).attr('required', true);
+  });
+});
