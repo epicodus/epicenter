d7cf50105543d2009bfb0827f81eba0766dfb8b4
Christopher Joy-Webb <chris@joywebb.com>
2015-12-01 10:26:11 -0800

    remove comments and decrease global font size to 1.7rem

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,15 +1,15 @@
 $(function() {
-  $('div').find('div.' + $('#student-nav li:first').attr('class')).show();  // shows the content of the first tab
-  $('div').find('div.' + $('#student-nav li:first').attr('class')).siblings('.student-div').hide();  // hides the content of all the other tabs
-  $('#student-nav li:first').addClass("active");  // sets the first tabs class to 'active'
+  $('div').find('div.' + $('#student-nav li:first').attr('class')).show();
+  $('div').find('div.' + $('#student-nav li:first').attr('class')).siblings('.student-div').hide();
+  $('#student-nav li:first').addClass("active");
 
   $('#student-nav li').each(function() {
     $(this).click(function() {
-      var matchingDiv = $('div').find('div.' + $(this).attr('class'));  // finds the div with the matching class
-      matchingDiv.show();  // shows the div that was clicked
-      matchingDiv.siblings('.student-div').hide();  // hides all other divs
-      $(this).addClass('active');  // adds 'active' class to clicked div
-      $(this).siblings().removeClass('active');  // removes 'active' class from anyother div
+      var matchingDiv = $('div').find('div.' + $(this).attr('class'));
+      matchingDiv.show();
+      matchingDiv.siblings('.student-div').hide();
+      $(this).addClass('active');
+      $(this).siblings().removeClass('active');
     });
   });
 });
