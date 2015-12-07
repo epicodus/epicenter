dd43714c28b2613b323fd245469504bafc330825
Christopher Joy-Webb <chris@joywebb.com>
2015-12-06 23:14:00 -0800

    fix hard tab formatting issue

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,14 +1,14 @@
 class Ticket < ActiveRecord::Base
-	acts_as_paranoid
+  acts_as_paranoid
 
-	belongs_to :course
+  belongs_to :course
 
-	validates :student_names, presence: true
-	validates :note, presence: true
-	validates :course_id, presence: true
-	validates :location, presence: true
+  validates :student_names, presence: true
+  validates :note, presence: true
+  validates :course_id, presence: true
+  validates :location, presence: true
 
-	def other_open_tickets
-		Ticket.where.not(id: id)
-	end
+  def other_open_tickets
+    Ticket.where.not(id: id)
+  end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,17 +1,17 @@
 describe Ticket do
-	it { should validate_presence_of :student_names }
-	it { should validate_presence_of :note }
-	it { should validate_presence_of :course_id }
-	it { should validate_presence_of :location }
-	it { should belong_to :course }
+  it { should validate_presence_of :student_names }
+  it { should validate_presence_of :note }
+  it { should validate_presence_of :course_id }
+  it { should validate_presence_of :location }
+  it { should belong_to :course }
 
-	describe '#other_open_tickets' do
-		let(:existing_ticket_1) { FactoryGirl.create(:ticket) }
-		let(:existing_ticket_2) { FactoryGirl.create(:ticket) }
-		let(:new_ticket) { FactoryGirl.create(:ticket) }
+  describe '#other_open_tickets' do
+    let(:existing_ticket_1) { FactoryGirl.create(:ticket) }
+    let(:existing_ticket_2) { FactoryGirl.create(:ticket) }
+    let(:new_ticket) { FactoryGirl.create(:ticket) }
 
-		it 'returns all open tickets excluding the current ticket' do
-			expect(new_ticket.other_open_tickets).to eq [existing_ticket_1, existing_ticket_2]
-		end
-	end
+    it 'returns all open tickets excluding the current ticket' do
+      expect(new_ticket.other_open_tickets).to eq [existing_ticket_1, existing_ticket_2]
+    end
+  end
 end
