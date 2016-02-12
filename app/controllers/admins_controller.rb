3628210959f369d742b6bd8b532f113ba7086ab2
Christopher Joy-Webb <chris@joywebb.com>
2016-02-12 13:45:48 -0800

    update redirect path when admins select new course from navbar

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,9 +2,9 @@ class AdminsController < ApplicationController
   def update
     @admin = Admin.find(params[:id])
     if @admin.update(admin_params)
-      redirect_to current_course_path(@admin.current_course), notice: "You have switched to #{@admin.current_course.description}."
+      redirect_to course_students_path(@admin.current_course), notice: "You have switched to #{@admin.current_course.description}."
     else
-      redirect_to current_course_path(@admin.current_course), alert: "Something went wrong."
+      redirect_to course_students_path(@admin.current_course), alert: "Something went wrong."
     end
   end
 
@@ -12,16 +12,5 @@ private
 
   def admin_params
     params.require(:admin).permit(:current_course_id)
-  end
-
-  def current_course_path(course)
-    course_referer = request.referer[/courses\/\d+\/(.*)/, 1]
-    if respond_to? "course_#{course_referer}_path"
-      send("course_#{course_referer}_path", course)
-    elsif respond_to? "#{course_referer}_course_path"
-      send("#{course_referer}_course_path", course)
-    else
-      :back
-    end
   end
 end
