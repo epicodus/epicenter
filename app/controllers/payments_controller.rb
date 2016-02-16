337d101734f42c8c0e9811962638bbfa63f4ac2c
Christopher Joy-Webb <chris@joywebb.com>
2016-02-15 16:05:31 -0800

    update PaymentsController actions for nested routing

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,14 +3,21 @@ class PaymentsController < ApplicationController
   before_filter :ensure_student_has_primary_payment_method
 
   def index
-    @payments = current_student.payments
-    if current_student.upfront_payment_due?
-      @payment = Payment.new(amount: current_student.upfront_amount_with_fees)
+    @student = Student.find(params[:student_id])
+    @payments = @student.payments
+    if @student.upfront_payment_due?
+      @payment = Payment.new(amount: @student.upfront_amount_with_fees)
     end
+  end
+
+  def show
+    @student = Student.find(params[:student_id])
+    @payment = Payment.find(params[:id])
   end
 
 private
   def ensure_student_has_primary_payment_method
-    redirect_to payment_methods_path if !current_student.primary_payment_method
+    @student = Student.find(params[:student_id])
+    redirect_to payment_methods_path if !@student.primary_payment_method
   end
 end
