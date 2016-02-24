651dce73e70707d249a3e8262bc28fc5c2ff43c4
chris <cunderwoodmn@gmail.com>
2016-02-24 14:01:31 -0800

    updates front end buttons, etc to payment-refund for maintainability

--- app/assets/javascripts/refunds.js
+++ app/assets/javascripts/refunds.js
@@ -1,7 +1,7 @@
 $(function() {
-  $('.payment-button').click(function() {
-    var paymentInput = $(this).parents('.input-group').find('.payment-input');
-    if (paymentInput.val().split('.')[1].length !== 2) {
+  $('.payment-refund-button').click(function() {
+    var inputAmount = $(this).parents('.input-group').find('.payment-refund-input');
+    if (inputAmount.val().split('.')[1].length !== 2) {
       alert('Please enter an amount that includes 2 decimal places.');
       return false;
     }
