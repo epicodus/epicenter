3025b255e35f4b09e4fd377ca868d8e36a0392dd
Christopher Joy-Webb <chris@joywebb.com>
2016-03-08 14:59:31 -0800

    add Company abilities model tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,3 +1,18 @@
 describe Company do
   it { should have_many :internships }
+
+  describe "abilities" do
+    let(:company) { FactoryGirl.create(:company) }
+    subject { Ability.new(company, '::1') }
+
+    context 'for companies' do
+      it { is_expected.to have_abilities(:manage, Company.new(id: company.id))}
+      it { is_expected.to_not have_abilities(:manage, Company.new)}
+    end
+
+    context 'for internships' do
+      it { is_expected.to have_abilities(:manage, Internship.new(company_id: company.id)) }
+      it { is_expected.to_not have_abilities(:manage, Internship.new) }
+    end
+  end
 end
