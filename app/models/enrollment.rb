7605c34d841d305ddbf8809b90a5bc1296a73d0a
Christopher Joy-Webb <chris@joywebb.com>
2016-03-30 10:24:05 -0700

    add Enrollment callback to check if student has credits remaining

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -5,4 +5,14 @@ class Enrollment < ActiveRecord::Base
   validates :course, presence: true
   validates :student, presence: true
   validates :student_id, uniqueness: { scope: :course_id }
+  before_save :check_student_credits, unless: ->(enrollment) { enrollment.student.plan.nil? }
+
+private
+
+  def check_student_credits
+    if student.courses.count >= student.plan.credits
+      errors.add(:student, 'has run out of credits and needs to pay for additional courses')
+      false
+    end
+  end
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -10,4 +10,19 @@ describe Enrollment do
       should validate_uniqueness_of(:student_id).scoped_to(:course_id)
     end
   end
+
+  describe 'checking student credits before enrolling' do
+    let(:course_1) { FactoryGirl.create(:course) }
+    let(:course_2) { FactoryGirl.create(:course) }
+    let(:course_3) { FactoryGirl.create(:course) }
+    let(:course_4) { FactoryGirl.create(:course) }
+    let(:course_5) { FactoryGirl.create(:course) }
+    let(:course_6) { FactoryGirl.create(:course) }
+
+    it 'does not enroll a student if the student has run out of credits' do
+      student = FactoryGirl.create(:student, courses: [course_1, course_2, course_3, course_4, course_5])
+      enrollment = Enrollment.new(student_id: student.id, course_id: course_6.id)
+      expect(enrollment.save).to be false
+    end
+  end
 end
