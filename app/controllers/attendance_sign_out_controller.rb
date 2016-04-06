380338df7489b41617ca636fd87b550dd83b52d6
Christopher Joy-Webb <chris@joywebb.com>
2016-04-06 16:31:30 -0700

    clean up some controller logic

--- app/controllers/attendance_sign_out_controller.rb
+++ app/controllers/attendance_sign_out_controller.rb
@@ -4,17 +4,9 @@ class AttendanceSignOutController < ApplicationController
     student = Student.find_by(email: params[:email].downcase)
     attendance_record = AttendanceRecord.find_by(date: Time.zone.now.to_date, student: student)
     if attendance_record && student.try(:valid_password?, params[:password])
-      authorize! :update, attendance_record
-      if attendance_record.update(attendance_record_params)
-        sign_out student
-        redirect_to sign_out_path, notice: "Goodbye #{attendance_record.student.name}. Your attendance record has been updated."
-      else
-        flash.now[:alert] = "Something went wrong: " + attendance_record.errors.full_messages.join(", ")
-        render 'new'
-      end
+      sign_out_student(student, attendance_record)
     elsif !attendance_record && student.try(:valid_password?, params[:password])
-      flash.now[:alert] = "You haven't signed in yet today."
-      render 'new'
+      alert_about_not_signing_in
     else
       flash.now[:alert] = 'Invalid email or password.'
       render 'new'
@@ -25,5 +17,21 @@ private
 
   def attendance_record_params
     params.permit(:signing_out, :student_id)
+  end
+
+  def sign_out_student(student, attendance_record)
+    authorize! :update, attendance_record
+    if attendance_record.update(attendance_record_params)
+      sign_out student
+      redirect_to sign_out_path, notice: "Goodbye #{attendance_record.student.name}. Your attendance record has been updated."
+    else
+      flash.now[:alert] = "Something went wrong: " + attendance_record.errors.full_messages.join(", ")
+      render 'new'
+    end
+  end
+
+  def alert_about_not_signing_in
+    flash.now[:alert] = "You haven't signed in yet today."
+    render 'new'
   end
 end
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,15 +4,21 @@ class OmniauthCallbacksController < ApplicationController
     response = request.env['omniauth.auth']
     user = User.find_by(email: response[:info][:email])
     if user.try(:authenticate_with_github, response[:uid])
-      sign_in user
-      if is_weekday? && IpLocation.is_local?(request.env['HTTP_CF_CONNECTING_IP'] || request.remote_ip) && user.is_a?(Student) && !user.signed_in_today?
-        attendance_record = AttendanceRecord.create(student: user)
-        redirect_to welcome_path, notice: 'Signed in successfully and attendance record created.'
-      else
-        redirect_to root_path, notice: 'Signed in successfully.'
-      end
+      sign_in_user(user)
     else
       redirect_to root_path, alert: 'Your GitHub and Epicenter credentials do not match.'
+    end
+  end
+
+private
+
+  def sign_in_user(user)
+    sign_in user
+    if is_weekday? && IpLocation.is_local?(request.env['HTTP_CF_CONNECTING_IP'] || request.remote_ip) && user.is_a?(Student) && !user.signed_in_today?
+      attendance_record = AttendanceRecord.create(student: user)
+      redirect_to welcome_path, notice: 'Signed in successfully and attendance record created.'
+    else
+      redirect_to root_path, notice: 'Signed in successfully.'
     end
   end
 end
