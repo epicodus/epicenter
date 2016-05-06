3e109d7e8cc3b8289b3f9c1eefb9433ea9358a21
Christopher Joy-Webb <chris@joywebb.com>
2016-05-06 16:36:25 -0700

    add custom create_signature_request_with_template method for EnrollmentAgreement; needs work

--- app/models/enrollment_agreement.rb
+++ app/models/enrollment_agreement.rb
@@ -1,13 +1,28 @@
 class EnrollmentAgreement < Signature
 
   attr_accessor :sign_url
-  after_initialize :create_signature_request
+  after_initialize :create_signature_request_with_template
 
 private
 
-  def create_signature_request
-    @subject = 'Sign to accept the Epicodus Enrollment Agreement'
-    @file = ENV['ENROLLMENT_AGREEMENT_DOCUMENT_URL']
-    super
+  def create_signature_request_with_template
+    signature_request = HelloSign.send_signature_request_with_template(
+      test_mode: ENV['HELLO_SIGN_TEST_MODE'],
+      client_id: ENV['HELLO_SIGN_CLIENT_ID'],
+      template_id: ENV['ENROLLMENT_AGREEMENT_TEMPLATE_ID'],
+      subject: 'Sign to accept the Epicodus Enrollment Agreement',
+      signers: [
+        {
+          email_address: student.email,
+          name: student.name,
+          role: 'Student'
+        }
+      ],
+      custom_fields: {
+        student_name: student.name,
+        program_start_date: Time.zone.now.to_date
+      }
+    )
+    self.signature_request_id = signature_request.data['signatures'].first['signature_id']
   end
 end
