48de44877f3a2a45d4866c5fa41c56f13dc5102d
Christopher Joy-Webb <chris@joywebb.com>
2016-06-09 16:22:50 -0700

    add InterviewAssignment model validations

--- app/models/interview_assignment.rb
+++ app/models/interview_assignment.rb
@@ -2,5 +2,7 @@ class InterviewAssignment < ActiveRecord::Base
   belongs_to :student
   belongs_to :internship
 
+  validates :student, presence: true
+  validates :internship, presence: true
   validates :internship_id, uniqueness: { scope: :student_id }
 end
--- spec/models/interview_assignment_spec.rb
+++ spec/models/interview_assignment_spec.rb
@@ -1,5 +1,7 @@
 describe InterviewAssignment do
   it { should belong_to :student }
   it { should belong_to :internship }
+  it { should validate_presence_of(:student) }
+  it { should validate_presence_of(:internship) }
   it { should validate_uniqueness_of(:internship_id).scoped_to(:student_id) }
 end
