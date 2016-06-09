78354ff3ce70f5554d740c3a61bb71f03f2e72e6
Christopher Joy-Webb <chris@joywebb.com>
2016-06-09 09:47:52 -0700

    add InterviewAssignment model uniqueness constraint and test

--- app/models/interview_assignment.rb
+++ app/models/interview_assignment.rb
@@ -1,4 +1,6 @@
 class InterviewAssignment < ActiveRecord::Base
   belongs_to :student
   belongs_to :internship
+
+  validates :internship_id, uniqueness: { scope: :student_id }
 end
--- spec/models/interview_assignment_spec.rb
+++ spec/models/interview_assignment_spec.rb
@@ -1,4 +1,5 @@
 describe InterviewAssignment do
   it { should belong_to :student }
   it { should belong_to :internship }
+  it { should validate_uniqueness_of(:internship_id).scoped_to(:student_id) }
 end
