dcd04620076df56a1dd9e52a2315ac9af8c8df1c
Christopher Joy-Webb <chris@joywebb.com>
2016-07-13 14:37:52 -0700

    refactor interview assignments create_multiple controller action

--- app/controllers/interview_assignments_controller.rb
+++ app/controllers/interview_assignments_controller.rb
@@ -3,10 +3,7 @@ class InterviewAssignmentsController < ApplicationController
   def create_multiple
     @course = Course.find(params[:course_id])
     @student = Student.find(params[:student_id])
-    interview_assignments = interview_assignment_params[:internship_id].map do |internship_id|
-      InterviewAssignment.create(student_id: @student.id, internship_id: internship_id)
-    end
-    if !interview_assignments.map(&:persisted?).include?(false)
+    if create_interview_assignments
       redirect_to course_student_path(@course, @student), notice: "Interview assignments added for #{@student.name}."
     else
       flash.now[:alert] = "Something went wrong: " + interview_assignments.first.errors.full_messages.join(", ")
@@ -24,6 +21,13 @@ class InterviewAssignmentsController < ApplicationController
   end
 
   private
+
+  def create_interview_assignments
+    interview_assignments = interview_assignment_params[:internship_id].map do |internship_id|
+      InterviewAssignment.create(student_id: @student.id, internship_id: internship_id)
+    end
+    !interview_assignments.map(&:persisted?).include?(false)
+  end
 
   def interview_assignment_params
     params[:interview_assignment][:internship_id] = params.dig(:interview_assignment).dig(:internship_id).select!(&:present?)
