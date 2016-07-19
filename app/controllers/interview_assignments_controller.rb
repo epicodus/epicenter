3fb2c172b31588a19207d8e412c62425813b7cee
Christopher Joy-Webb <chris@joywebb.com>
2016-07-19 10:52:45 -0700

    add acceptance test for unsuccessfully adding interview assignment

--- app/controllers/interview_assignments_controller.rb
+++ app/controllers/interview_assignments_controller.rb
@@ -3,7 +3,10 @@ class InterviewAssignmentsController < ApplicationController
   def create_multiple
     @course = Course.find(params[:course_id])
     @student = Student.find(params[:student_id])
-    if create_interview_assignments
+    interview_assignments = interview_assignment_params[:internship_id].map do |internship_id|
+      InterviewAssignment.create(student_id: @student.id, internship_id: internship_id, course_id: @course.id)
+    end
+    if interview_assignments_created_successfully(interview_assignments)
       redirect_to course_student_path(@course, @student), notice: "Interview assignments added for #{@student.name}."
     else
       flash.now[:alert] = "Something went wrong: " + interview_assignments.first.errors.full_messages.join(", ")
@@ -28,10 +31,7 @@ class InterviewAssignmentsController < ApplicationController
 
   private
 
-  def create_interview_assignments
-    interview_assignments = interview_assignment_params[:internship_id].map do |internship_id|
-      InterviewAssignment.create(student_id: @student.id, internship_id: internship_id, course_id: @course.id)
-    end
+  def interview_assignments_created_successfully(interview_assignments)
     !interview_assignments.map(&:persisted?).include?(false)
   end
 
