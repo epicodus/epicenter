feaf492f567c45136f5f5ec1aab1fab6a3a37a5b
Christopher Joy-Webb <chris@joywebb.com>
2016-07-29 10:01:46 -0700

    add philadelphia public ip address for attendance

--- app/models/ip_location.rb
+++ app/models/ip_location.rb
@@ -2,11 +2,11 @@ class IpLocation
 
   def self.is_local?(ip)
     ip = IPAddr.new(ip)
-    local_ip_ranges = ["::1", ENV['SCHOOL_IP_ADDRESS'], ENV['SCHOOL_WIFI_IP_ADDRESS'], ENV['SEATTLE_WIFI_IP_ADDRESS']]
+    local_ip_ranges = ["::1", ENV['SCHOOL_IP_ADDRESS'], ENV['SCHOOL_WIFI_IP_ADDRESS'], ENV['SEATTLE_WIFI_IP_ADDRESS'], ENV['PHILADELPHIA_WIFI_IP_ADDRESS']]
     local_ip_ranges.any? { |range| IPAddr.new(range).include?(ip) }
   end
 
   def self.is_local_computer?(ip)
-    IPAddr.new(ip) == ENV['SCHOOL_IP_ADDRESS'] || IPAddr.new(ip) == ENV['SEATTLE_WIFI_IP_ADDRESS']
+    IPAddr.new(ip) == ENV['SCHOOL_IP_ADDRESS'] || IPAddr.new(ip) == ENV['SEATTLE_WIFI_IP_ADDRESS'] || IPAddr.new(ip) == ENV['PHILADELPHIA_WIFI_IP_ADDRESS']
   end
 end
