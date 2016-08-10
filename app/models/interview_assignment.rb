71cab2c7247e07914a5c987d9fde2308d2b00141
Christopher Joy-Webb <chris@joywebb.com>
2016-08-10 14:07:48 -0700

    convert student name to link in company rankings view partial

--- app/models/interview_assignment.rb
+++ app/models/interview_assignment.rb
@@ -10,6 +10,6 @@ class InterviewAssignment < ActiveRecord::Base
 
   scope :order_by_internship_name, -> { includes(internship: :company).order('internships.name') }
   scope :for_course, ->(course) { where(course_id: course.id) }
-  scope :for_internship, ->(internship) { where(internship_id: internship.id).order(:ranking_from_company) }
+  scope :for_internship, ->(internship) { where(internship_id: internship.id).includes(:student).order(:ranking_from_company) }
   scope :with_feedback_from_company, -> { where.not(feedback_from_company: nil) }
 end
