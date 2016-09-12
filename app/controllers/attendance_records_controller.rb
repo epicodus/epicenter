9035ab15fd97fa66db2976d77533a9597c32e80e
Christopher Joy-Webb <chris@joywebb.com>
2016-09-12 14:52:44 -0700

    add pagination to attendance records list

--- app/controllers/attendance_records_controller.rb
+++ app/controllers/attendance_records_controller.rb
@@ -1,6 +1,7 @@
 class AttendanceRecordsController < ApplicationController
   def index
     @student = Student.find(params[:student_id])
+    @attendance_days = Kaminari.paginate_array(@student.courses.non_internship_courses.total_class_days_until(Time.zone.now.to_date)).page(params[:page]).per(15)
     authorize! :manage, @student
   end
 end
