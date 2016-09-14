3be274599a29c7eeddf8113e3616825a311da35b
Christopher Joy-Webb <chris@joywebb.com>
2016-09-14 16:41:23 -0700

    refactor SessionsController create action override

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,9 @@ class Users::SessionsController < Devise::SessionsController
     params[:user][:email] = params[:user][:email].downcase
     user = User.find_by(email: params[:user][:email])
     if user.try(:valid_password?, params[:user][:password])
-      sign_in_admin_or_company_or_student(user)
+      request.env["devise.mapping"] = Devise.mappings[user.class.to_s.downcase.to_sym]
+      sign_in user
+      redirect_to root_path, notice: 'Signed in successfully.'
     else
       super
     end
@@ -15,17 +17,5 @@ private
 
   def redirect_if_logged_in
     redirect_to after_sign_in_path_for(current_user) if current_user
-  end
-
-  def sign_in_admin_or_company_or_student(user)
-    if user.is_a? Admin
-      request.env["devise.mapping"] = Devise.mappings[:admin]
-    elsif user.is_a? Company
-      request.env["devise.mapping"] = Devise.mappings[:company]
-    elsif user.is_a? Student
-      request.env["devise.mapping"] = Devise.mappings[:student]
-    end
-    sign_in user
-    redirect_to root_path, notice: 'Signed in successfully.'
   end
 end
