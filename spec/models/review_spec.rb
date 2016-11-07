056a975a3af11d9f3eb285cf3368bcff7acd5b29
Mike Goren <mike@mortalwombat.net>
2016-11-07 10:24:04 -0800

    fix code review link in email sent to students after review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -26,7 +26,7 @@ describe Review do
         { :from => ENV['FROM_EMAIL_REVIEW'],
           :to => student.email,
           :subject => "Code review reviewed",
-          :text => "Hi #{student.name}. Your #{submission.code_review.title} code has been reviewed. You can view it at #{Rails.application.routes.url_helpers.code_review_url(submission.code_review)}."
+          :text => "Hi #{student.name}. Your #{submission.code_review.title} code has been reviewed. You can view it at #{Rails.application.routes.url_helpers.course_code_review_url(submission.code_review.course, submission.code_review)}."
         }
       )
     end
