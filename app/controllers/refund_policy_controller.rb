b161131a2f176d639101a98c65a5bc8ceb917208
Mike Goren <mike@mortalwombat.net>
2016-12-18 15:36:58 -0500

    simplify signature flow

--- app/controllers/refund_policy_controller.rb
+++ app/controllers/refund_policy_controller.rb
@@ -3,6 +3,6 @@ class RefundPolicyController < SignaturesController
   before_filter :authenticate_student!
 
   def new
-    super(CodeOfConduct, RefundPolicy, 'enrollment_agreement', :new)
+    super(RefundPolicy)
   end
 end
