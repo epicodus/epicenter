6510af8a9559e85a22be8a8cc71401843ce476dd
Mike Goren <mike@mortalwombat.net>
2017-01-05 12:55:38 -0800

    update dates that display on transcripts and certificates

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -2,6 +2,6 @@ class TranscriptsController < ApplicationController
   authorize_resource
 
   def show
-    @completed_courses = current_student.courses.previous_courses.non_internship_courses.where.not(description: "* Placement Test").order(:start_date)
+    @completed_courses = current_student.courses.previous_courses.where.not(description: "* Placement Test").order(:start_date)
   end
 end
