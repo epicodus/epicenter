fc2ce236e7a94c143ab9a35ad39610ee5a20017e
Mike Goren <mike@mortalwombat.net>
2017-01-08 14:37:10 -0800

    show transcript only if at least 1 class completed

--- spec/features/transcript_pages_spec.rb
+++ spec/features/transcript_pages_spec.rb
@@ -1,14 +1,25 @@
-feature "viewing transcript" do
-  context "before class is over" do
-    it "doesn't show link to print certificate" do
+feature "viewing transcript & certificate" do
+  context "before first class is over" do
+    it "doesn't show transcript" do
       student = FactoryGirl.create(:student)
       login_as(student, scope: :student)
       visit edit_student_registration_path
-      expect(page).to_not have_link "View/print transcript"
+      expect(page).to_not have_link "View transcript"
+      visit transcript_path
+      expect(page).to have_content "Transcript not yet available."
+    end
+
+    it "doesn't show certificate" do
+      student = FactoryGirl.create(:student)
+      login_as(student, scope: :student)
+      visit edit_student_registration_path
+      expect(page).to_not have_link "View certificate of completion"
+      visit certificate_path
+      expect(page).to have_content "Certificate not yet available."
     end
   end
 
-  context "after class ends" do
+  context "after completed 1 class" do
     it "allows student to view their transcript" do
       course = FactoryGirl.create(:past_course)
       student = FactoryGirl.create(:student, course: course)
@@ -17,6 +28,54 @@ feature "viewing transcript" do
       click_link "View transcript"
       expect(page).to have_content "Transcript"
       expect(page).to have_content student.name
+    end
+
+    it "doesn't show certificate" do
+      course = FactoryGirl.create(:past_course)
+      student = FactoryGirl.create(:student, course: course)
+      login_as(student, scope: :student)
+      visit edit_student_registration_path
+      expect(page).to_not have_link "View certificate of completion"
+      visit certificate_path
+      expect(page).to have_content "Certificate not yet available."
+    end
+  end
+
+  context "after internship class ends" do
+    it "allows student to view their transcript" do
+      course = FactoryGirl.create(:past_course, internship_course: true)
+      student = FactoryGirl.create(:student, course: course)
+      login_as(student, scope: :student)
+      visit edit_student_registration_path
+      click_link "View transcript"
+      expect(page).to have_content "Transcript"
+      expect(page).to have_content student.name
+    end
+
+    it "allows student to view their certificate if passed all code reviews", :stub_mailgun do
+      course = FactoryGirl.create(:past_course, internship_course: true)
+      student = FactoryGirl.create(:student, course: course)
+      code_review = FactoryGirl.create(:code_review, course: course)
+      submission = FactoryGirl.create(:submission, code_review: code_review, student: student)
+      FactoryGirl.create(:passing_review, submission: submission)
+      login_as(student, scope: :student)
+      visit edit_student_registration_path
+      click_link "View certificate of completion"
+      expect(page).to have_content "Epicodus Certificate of Completion"
+      expect(page).to have_content student.name
+    end
+
+    it "doesn't show certificate if student has any failing code reviews", :stub_mailgun do
+      course = FactoryGirl.create(:past_course, internship_course: true)
+      student = FactoryGirl.create(:student, course: course)
+      code_review = FactoryGirl.create(:code_review, course: course)
+      submission = FactoryGirl.create(:submission, code_review: code_review, student: student)
+      FactoryGirl.create(:failing_review, submission: submission)
+      login_as(student, scope: :student)
+      visit edit_student_registration_path
+      expect(page).to_not have_link "View certificate of completion"
+      visit certificate_path
+      expect(page).to have_content "Certificate not yet available."
     end
 
     it 'shows a breakdown of how they did on each code review', :stub_mailgun do
