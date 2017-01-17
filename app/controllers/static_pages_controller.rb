0fe3a755a4efa72e92b582b063b159d1ac0ed25c
Mike Goren <mike@mortalwombat.net>
2017-01-16 16:42:21 -0800

    make queue link on welcome page IP-dependent

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def show
+    if IpLocation.is_local_computer_seattle?(request.env['HTTP_CF_CONNECTING_IP'] || request.remote_ip)
+      @queue_url = 'https://seattle-help.epicodus.com'
+    else
+      @queue_url = 'https://help.epicodus.com'
+    end
   end
 end
