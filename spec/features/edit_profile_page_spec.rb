ffe822867ea55364a986731e457a95284031f80a
Mike Goren <mike@mortalwombat.net>
2017-02-15 15:12:01 -0800

    update CRM when email is updated in Epicenter

--- spec/features/edit_profile_page_spec.rb
+++ spec/features/edit_profile_page_spec.rb
@@ -1,21 +1,46 @@
 feature 'Student edits their profile' do
+  let(:student) { FactoryGirl.create(:user_with_all_documents_signed, email: "example@example.com") }
+  let(:close_io_client) { Closeio::Client.new(ENV['CLOSE_IO_API_KEY'], false) }
+
+  before do
+    login_as(student, scope: :student)
+    visit edit_student_registration_path
+  end
+
   context 'with the correct password' do
     it "successfully updates information" do
-      student = FactoryGirl.create(:user_with_all_documents_signed)
-      login_as(student, scope: :student)
-      visit edit_student_registration_path
       fill_in 'Name', with: 'New Name'
       fill_in 'Current password', with: student.password
       click_on 'Update'
       expect(page).to have_content "You updated your account successfully."
     end
+
+    it "successfully updates email", :vcr do
+      fill_in 'Email', with: 'second-email@example.com'
+      fill_in 'Current password', with: student.password
+      click_on 'Update'
+      expect(page).to have_content "You updated your account successfully."
+      student.update_close_email("example@example.com") # reset for use by other tests
+    end
+
+    it "does not update email if new email address is invalid", :vcr do
+      fill_in 'Email', with: 'invalid@invalid'
+      fill_in 'Current password', with: student.password
+      click_on 'Update'
+      expect(page).to have_content "Invalid email address."
+    end
+
+    it "does not update email if existing address not found in Close", :vcr do
+      student.update(email: "no_close_entry@example.com")
+      fill_in 'Email', with: 'second-email@example.com'
+      fill_in 'Current password', with: student.password
+      click_on 'Update'
+      expect(page).to have_content "was not found"
+    end
   end
 
   context 'with the incorrect password' do
     it "shows an error" do
-      student = FactoryGirl.create(:user_with_all_documents_signed)
-      login_as(student, scope: :student)
-      visit edit_student_registration_path
       fill_in 'Name', with: 'New Name'
       fill_in 'Current password', with: 'madeUpPassword'
       click_on 'Update'
