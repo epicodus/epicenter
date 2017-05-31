4f3e4d3e2745126e16277fa7cef1a71102eb318f
Mike Goren <mike@mortalwombat.net>
2017-05-31 16:09:25 -0700

    update code review display, add blurb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518224212) do
+ActiveRecord::Schema.define(version: 20170531202423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -252,6 +252,7 @@ ActiveRecord::Schema.define(version: 20170518224212) do
     t.datetime "updated_at"
     t.boolean  "needs_review"
     t.integer  "times_submitted"
+    t.string   "blurb"
   end
 
   add_index "submissions", ["code_review_id"], name: "index_submissions_on_code_review_id", using: :btree
