ea44ad54050403be9e3f6863008b92e4ba05b81c
Mike Goren <mike@mortalwombat.net>
2018-04-23 14:24:16 -0700

    update gems

--- app/models/student_internship_agreement.rb
+++ app/models/student_internship_agreement.rb
@@ -5,7 +5,7 @@ class StudentInternshipAgreement < Signature
 
   def self.create_from_signature_id(signature_id)
     student = Signature.find_by(signature_id: signature_id).student
-    code_review = student.internship_course.code_reviews.find_by(title: "Sign internship agreement")
+    code_review = student.internship_course.code_reviews.find_by(title: "Sign internship agreement") || student.internship_course.code_reviews.find_by(title: "Sign Internship Agreement")
     submission = Submission.find_or_create_by(student: student, code_review: code_review)
     review = submission.reviews.create(note: "provisionally marked as completed", student_signature: "n/a", admin_id: 198)
     Grade.create(score: Score.find_by(value: 3), objective: code_review.objectives.first, review: review)
