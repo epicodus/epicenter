bfd0c0bc887bf838832674813f0f15b447e53195
Mike Goren <mike@mortalwombat.net>
2018-08-13 12:43:07 -0700

    seed plan order

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,11 +1,10 @@
 class Plan < ApplicationRecord
-  scope :active, -> { where(archived: nil).order(:name) }
+  scope :active, -> { where(archived: nil).order(:order) }
   scope :standard, -> { where(standard: true) }
   scope :upfront, -> { where(upfront: true) }
   scope :loan, -> { where(loan: true) }
   scope :parttime, -> { where(parttime: true) }
   scope :fulltime, -> { where(parttime: nil) }
-  scope :rates_2018, -> { where(start_date: Time.new(2017, 9, 5).to_date) }
 
   has_many :students
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_001949) do
+ActiveRecord::Schema.define(version: 2018_08_13_194200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -234,6 +234,7 @@ ActiveRecord::Schema.define(version: 2018_08_11_001949) do
     t.boolean "upfront"
     t.string "description"
     t.string "short_name"
+    t.integer "order"
   end
 
   create_table "ratings", id: :serial, force: :cascade do |t|
