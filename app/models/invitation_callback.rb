a63f307f01b3f917a26e1f90e692a74ffef57310
Mike Goren <mike@mortalwombat.net>
2018-10-18 11:02:41 -0700

    add logging for invitation callback

--- app/models/invitation_callback.rb
+++ app/models/invitation_callback.rb
@@ -3,7 +3,9 @@ class InvitationCallback
 
   def initialize(params)
     email = params[:email]
+    Rails.logger.info "Invitation Callback: inviting #{email}"
     if CrmLead.lead_exists?(email)
+      Rails.logger.info "Invitation Callback: found email in CRM"
       crm_lead = CrmLead.new(email)
       student = Student.invite!(email: email, name: crm_lead.name) do |u|
         u.skip_invitation = true
@@ -14,6 +16,7 @@ class InvitationCallback
       student.update(office: student.course.office)
       crm_lead.update({ 'custom.Epicenter - Raw Invitation Token': student.raw_invitation_token })
     else
+      Rails.logger.info "Invitation Callback: not found in CRM"
       raise CrmError, "Invitation callback: CRM lead not found for #{email}"
     end
   end
