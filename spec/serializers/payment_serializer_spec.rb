fd2cefcbbd134996ffc8d69a9aac829eefaa864e
Mike Goren <mike@mortalwombat.net>
2018-11-14 16:17:11 -0800

    set refund date to start date if 1st 5 weeks

--- spec/serializers/payment_serializer_spec.rb
+++ spec/serializers/payment_serializer_spec.rb
@@ -14,13 +14,13 @@ describe PaymentSerializer, :stripe_mock, :stub_mailgun, :vcr do
 
   it 'includes the expected attributes for a refund' do
     student = FactoryBot.create(:student_with_credit_card)
-    payment = FactoryBot.create(:payment_with_credit_card, student: student, amount: 600_00, refund_amount: 100_00, refund_date: Date.today)
+    payment = FactoryBot.create(:payment_with_credit_card, student: student, amount: 600_00, refund_amount: 100_00, refund_date: student.course.start_date)
     serialized_payment = PaymentSerializer.new(payment).as_json
     expect(serialized_payment[:amount]).to eq 60000
     expect(serialized_payment[:refund_amount]).to eq 10000
     expect(serialized_payment[:email]).to eq student.email
     expect(serialized_payment[:office]).to eq student.office.short_name
-    expect(serialized_payment[:start_date]).to eq Date.today.to_s
+    expect(serialized_payment[:start_date]).to eq student.course.start_date.to_s
     expect(serialized_payment[:end_date]).to eq student.course.end_date.to_s
   end
 end
