372670e8ce3e53c6283aad465118f75f2b8d8f42
Mike Goren <mike@mortalwombat.net>
2019-01-08 12:57:23 -0800

    debug: log remote IP on attendance requests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
 
   def show
+    Rails.logger.info "Requesting IP HTTP_CF_CONNECTING_IP: #{request.env['HTTP_CF_CONNECTING_IP']}"
+    Rails.logger.info "Requesting IP remote_ip: #{request.remote_ip}"
     if IpLocation.is_local_computer_seattle?(request.env['HTTP_CF_CONNECTING_IP'] || request.remote_ip)
       @queue_url = 'https://seattle-help.epicodus.com'
     else
