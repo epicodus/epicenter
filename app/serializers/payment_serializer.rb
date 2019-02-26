1bb8c1350dac0a847347aafa6213bedc1b71f83e
Mike Goren <mike@mortalwombat.net>
2019-02-26 10:46:19 -0800

    bugfix: payment serializer updated_at

--- app/serializers/payment_serializer.rb
+++ app/serializers/payment_serializer.rb
@@ -6,7 +6,7 @@ class PaymentSerializer < ActiveModel::Serializer
     payment = object
     student = Student.with_deleted.find_by_id(payment.student_id)
     hash[:created_at] = payment.created_at.to_date.to_s
-    hash[:updated_at] = payment.created_at.to_date.to_s unless payment.updated_at == payment.created_at
+    hash[:updated_at] = payment.updated_at.to_date.to_s unless payment.updated_at == payment.created_at
     hash[:email] = student.email
     hash[:office] = student.office.short_name
     hash[:start_date] = payment.refund_date.try(:to_s) || payment.description[0..9]
