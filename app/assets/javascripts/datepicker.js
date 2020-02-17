4a3720f11e7a087a880cf235e0486fe0ef23a145
Mike Goren <mike@mortalwombat.net>
2020-02-17 12:22:33 -0500

    allow date picker to change sundays

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -2,8 +2,7 @@ $(function() {
   $('.custom-datepicker').datepicker({
     format: "yyyy-mm-dd",
     multidate: true,
-    todayHighlight: true,
-    daysOfWeekDisabled: '0,6'
+    todayHighlight: true
   });
 
   $(".custom-datepicker").on("changeDate", function() {
