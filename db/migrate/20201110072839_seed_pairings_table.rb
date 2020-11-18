5279cc5823bdf2a74e06d94cc094e5dfe7c4b4a3
Mike Goren <mike@mortalwombat.net>
2020-11-17 18:55:12 -0800

    remove long migration from pairings refactor

--- db/migrate/20201110072839_seed_pairings_table.rb
+++
@@ -1,9 +0,0 @@
-class SeedPairingsTable < ActiveRecord::Migration[5.2]
-  def change
-    AttendanceRecord.where.not(old_pair_ids: []).each do |ar|
-      ar.old_pair_ids.each do |pair_id|
-        ar.pairings << Pairing.new(pair_id: pair_id)
-      end
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_005113) do
+ActiveRecord::Schema.define(version: 2020_11_09_215934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_11_005113) do
     t.datetime "signed_out_time"
     t.string "station"
     t.boolean "ignore"
+    t.integer "old_pair_ids", default: [], array: true
     t.index ["created_at"], name: "index_attendance_records_on_created_at"
     t.index ["student_id"], name: "index_attendance_records_on_student_id"
     t.index ["tardy"], name: "index_attendance_records_on_tardy"
