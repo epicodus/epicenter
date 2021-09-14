22eff7dd660b943ab41e4b2ede9e3ca4f7e4f66a
Mike Goren <mike@mortalwombat.net>
2021-09-13 18:06:06 -0700

    create static link that redirects students to course containing specific journal

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,17 +1,28 @@
 class JournalsController < ApplicationController
-  before_action { redirect_to root_path, alert: 'You are not authorized to access this page.' unless current_admin }
+  before_action { redirect_to root_path, alert: 'You are not authorized to access this page.' unless current_admin || current_student }
 
   def index
-    @journals = CodeReview.where(journal: true)
-    if params[:cohort_id]
-      @cohort = Cohort.find(params[:cohort_id])
-      @journals = @journals.where(course: @cohort.courses)
+    if current_admin
+      @journals = CodeReview.where(journal: true)
+      if params[:cohort_id]
+        @cohort = Cohort.find(params[:cohort_id])
+        @journals = @journals.where(course: @cohort.courses)
+      end
+      if params[:title]
+        @title = params[:title]
+        @journals = @journals.where(title: @title)
+        @submissions = Submission.where(code_review: @journals).reorder(created_at: :desc)
+      end
+      redirect_to cohort_path(@cohort) if @cohort.present? && @title.nil?
+    elsif current_student
+      valid_journal_titles = CodeReview.where(journal: true).pluck(:title).uniq
+      title = params[:title] if valid_journal_titles.include?(params[:title])
+      journal = CodeReview.where(journal: true).where(title: title).where(course: current_student.courses).last
+      if journal
+        redirect_to course_student_path(journal.course, current_student)
+      else
+        redirect_to root_path, alert: 'You are not authorized to access this page.'
+      end
     end
-    if params[:title]
-      @title = params[:title]
-      @journals = @journals.where(title: @title)
-      @submissions = Submission.where(code_review: @journals).reorder(created_at: :desc)
-    end
-    redirect_to cohort_path(@cohort) if @cohort.present? && @title.nil?
   end
 end
