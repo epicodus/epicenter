6f81bc92cea5400292266f3f0a9b8b370a05ca46
Mike Goren <mike@mortalwombat.net>
2023-09-13 23:41:44 -0700

    remove no-sandbox option

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,6 @@ include ActiveSupport::Testing::TimeHelpers
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
   options.add_argument('--remote-debugging-port=9222')
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
