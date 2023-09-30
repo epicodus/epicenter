901f7c85de284a8ed8e58d5eb0c29ba59854a868
Mike Goren <mike@epicodus.com>
2023-09-29 19:56:24 -0700

    fix brittle check-in count test

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -2,5 +2,5 @@ class Checkin < ApplicationRecord
   belongs_to :student, class_name: 'User'
   belongs_to :admin, class_name: 'User'
 
-  scope :week, ->(date = Date.today) { where('created_at BETWEEN ? AND ?', date.beginning_of_week(:saturday), date.end_of_week(:saturday) + 1.day) }
+  scope :week, ->(date = Date.today) { where(created_at: date.beginning_of_week(:saturday).beginning_of_day..(date.end_of_week(:saturday) + 1.day).end_of_day) }
 end
