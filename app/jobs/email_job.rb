3c9d1a1d7ef8f9ddb5f1ae75b78e36094af1b578
Mike Goren <mike@mortalwombat.net>
2024-10-25 16:37:14 -0700

    disable email job queueing

--- app/jobs/email_job.rb
+++ app/jobs/email_job.rb
@@ -1,7 +1,13 @@
-class EmailJob < ApplicationJob
-  queue_as :default
-
-  def perform(args)
+# disable queueing!!
+class EmailJob
+  def self.perform_later(args)
     EmailClient.create.send_message(ENV['MAILGUN_DOMAIN'], args)
   end
 end
+
+# class EmailJob < ApplicationJob
+#   queue_as :default
+#   def perform(args)
+#     EmailClient.create.send_message(ENV['MAILGUN_DOMAIN'], args)
+#   end
+# end
